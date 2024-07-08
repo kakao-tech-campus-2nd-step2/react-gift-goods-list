86b4def4d75c252d13bc3006111612497f57ae05
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-07-09 01:00:44 +0900

    feat: oas.yaml파일을 참고하여 Request, Response Type 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,16 @@
+export type ProductOrderRequestBody = {
+  productId: number;
+  productOptionId: number;
+  productQuantity: number;
+  messageCardTemplateId: number;
+  messageCardTextMessage: string;
+  senderId: number;
+  receiverId: number;
+  hasCashReceipt: boolean;
+  cashReceiptType: 'PERSONAL' | 'BUSINESS';
+  cashReceiptNumber: string;
+};
+
 export type ThemeData = {
   id: number;
   key: string;
@@ -12,7 +25,7 @@ export type RankingFilterOption = {
   rankType: 'MANY_WISH' | 'MANY_RECEIVE' | 'MANY_WISH_RECEIVE';
 };
 
-export type GoodsData = {
+export type ProductData = {
   id: number;
   name: string;
   imageURL: string;
@@ -31,3 +44,74 @@ export type GoodsData = {
     imageURL: string;
   };
 };
+
+export type ProductDetailData = {
+  isAccessableProductPage: boolean;
+  review: {
+    averageRating: number;
+    totalReviewCount: number;
+  };
+  productDescription: {
+    images: string[];
+  };
+  productDetailInfo: {
+    announcements: {
+      displayOrder: number;
+      name: string;
+      value: string;
+    }[];
+    terms: {
+      displayOrder: number;
+      title: string;
+      description: string;
+    }[];
+  };
+};
+
+export type MessageCardTemplateData = {
+  id: number;
+  defaultTextMessage: string;
+  thumbURL: string;
+  imageURL: string;
+};
+
+export type MyAccountInfoData = {
+  id: number;
+  name: string;
+  birthday?: string;
+  profileImageURL: string;
+  point: number;
+};
+
+export type GetProductRankingsResponse = {
+  products: ProductData[];
+};
+
+export type GetGiftThemesResponse = {
+  themes: ThemeData[];
+};
+
+export type PaginatedProductResponse = {
+  products: ProductData[];
+  nextPageToken?: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+};
+
+export type GetProductsByThemeResponse = PaginatedProductResponse;
+
+export type GetProductDetailResponse = ProductDetailData & ProductData;
+
+export type GetMessageCardTemplatesResponse = {
+  templates: MessageCardTemplateData[];
+};
+
+export type GetMyAccountInfoResponse = MyAccountInfoData;
+
+export type GetMyWishListResponse = PaginatedProductResponse;
+
+export type ChargeMyPointsResponse = void;
+
+export type OrderProductResponse = void;

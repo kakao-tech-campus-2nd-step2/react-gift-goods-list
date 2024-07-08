99dc11dd42df1ec019503f1e60a859fa69491cc7
Jisu Lee <dlwltn0430@naver.com>
2024-07-09 00:28:11 +0900

    feat: ThemeCategorySection 구현

---
+++ src/api/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://react-gift-mock-api-dlwltn0430.vercel.app/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default instance;

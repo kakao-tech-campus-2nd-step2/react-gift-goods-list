3a55bca3868ea4840a16dac92474582a2e25b565
박건규 <geongyu09@gmail.com>
2024-07-09 20:18:18 +0900

    feat: theme 페이지 상붐 목록 api 적용

--- src/apis/theme/index.ts
+++ src/apis/theme/index.ts
@@ -7,8 +7,3 @@ export const getThemes = async (): Promise<Theme[]> => {
   const res = await instance.get(API.THEMES);
   return res.data!.themes;
 };
-
-export const getThemeDetail = async (themeKey: string): Promise<Theme> => {
-  const res = await instance.get(`${API.THEMES}/${themeKey}`);
-  return res.data!.theme;
-};

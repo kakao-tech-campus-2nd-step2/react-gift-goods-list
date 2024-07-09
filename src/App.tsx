9d3b10c51ba3a4edf74592f0a3119cb8a3e96a64
dandamdandam <glue0440@gmail.com>
2024-07-09 15:39:06 +0900

    init: init project
    2주차 내용 불러옴

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import { AuthProvider } from './provider/Auth';
-import { Routes } from './routes';
+import { useEffect, useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import AuthContext from './context/AuthContext';
+import Error from './pages/Error';
+import Login from './pages/Login';
+import Main from './pages/Main';
+import MyAccount from './pages/MyAccount';
+import Theme from './pages/Theme';
 
 const App = () => {
-  return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
-  );
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    useEffect(() => {
+        const authToken = sessionStorage.getItem('authToken');
+        setIsAuthenticated(!!authToken);
+    }, [isAuthenticated]);
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+            <RouterProvider router={router} />
+            <Footer />
+        </AuthContext.Provider>
+    );
 };
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+    {
+        path: '/my-account',
+        element: <MyAccount />,
+    },
+    {
+        path: '/error/:http_status',
+        element: <Error />,
+    },
+    {
+        path: '/theme/:themeKey',
+        element: <Theme />,
+    },
+]);
 
 export default App;

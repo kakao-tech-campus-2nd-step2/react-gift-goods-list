a15a16b2aa8a80ef5da0db63bf87f9cb9476e338
eunjin <eunjin0321@naver.com>
2024-07-10 02:06:56 +0900

    add : 실시간 급상승 랭킹 api 연동

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import type { ThemeData } from '@/types';
+import type { GoodsData, RankingFilterOption,ThemeData } from '@/types';
 
 const api = axios.create({
   baseURL: 'https://react-gift-mock-api-eunjin.vercel.app/',});
@@ -8,6 +8,13 @@ const api = axios.create({
 export const getTheme = async (): Promise<ThemeData[]> => {
   const response = await api.get<{ themes: ThemeData[] }>('/api/v1/themes');
   return response.data.themes;
+};
+
+export const getRankingGoods = async (filterOption: RankingFilterOption): Promise<GoodsData[]> => {
+  const response = await api.get<{ products: GoodsData[] }>('/api/v1/ranking/products', {
+    params: filterOption,
+  });
+  return response.data.products;
 };
 
 

0f75d2284e1d45fcba4ac5b5800e90e0dbe62c7c
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-07-09 15:50:30 +0900

    feat: set theme category section api

--- src/libs/api.tsx
+++ src/libs/api.tsx
@@ -4,19 +4,18 @@ const API_BASE_URL = 'https://kakao-tech-campus-mock-server.vercel.app';
 
 export const getThemes = async () => {
   const response = await axios.get(`${API_BASE_URL}/api/v1/themes`);
-  console.log(response.data);
+
   return response.data;
 };
 
-// export const getRanking = async (params: {
-//     targetType?: string;
-//     rankType?: string;
-//   }): Promise<RankingProductsResponse> => {
-//     const response = await axios.get(`${API_BASE_URL}/ranking-products`, { params });
-//     return response.data;
-// };
+export const getRankingProducts = async (params: { targetType: string; rankType: string }) => {
+  const response = await axios.get(`${API_BASE_URL}/api/v1/ranking/products`, { params });
+  return response.data;
+};
 
-export const getTheme = async (themeKey: string) => {
-  const response = await axios.get(`${API_BASE_URL}/api/v1/themes/${themeKey}/products`);
+export const getTheme = async (themeKey: string, maxResults: number = 20) => {
+  const response = await axios.get(`${API_BASE_URL}/api/v1/themes/${themeKey}/products`, {
+    params: { maxResults },
+  });
   return response.data;
 };

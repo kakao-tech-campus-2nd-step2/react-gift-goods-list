69522fec25213fa2485e734138004a1c74841b78
yunseong0404 <333665@naver.com>
2024-07-10 02:55:04 +0900

    fix: theme header 구현

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,13 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
-import { getCurrentTheme, ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
+import { ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
+import { getCurrentTheme } from '@/components/features/Theme/ThemeHeroSection';
 import { RouterPath } from '@/routes/path';
-import { ThemeMockList } from '@/types/mock';
+import type { ThemeData } from '@/types';
 
 export const ThemePage = () => {
   const { themeKey = '' } = useParams<{ themeKey: string }>();
-  const currentTheme = getCurrentTheme(themeKey, ThemeMockList);
+  const [Themes, setThemes] = useState<ThemeData[]>([]);
+
+  useEffect(() => {
+    const fetchThemes = async () => {
+      try {
+        const response = await axios.get(
+          'https://react-gift-mock-api-ten.vercel.app/api/v1/themes',
+        );
+        setThemes(response.data.themes);
+      } catch (error) {
+        console.error('Error fetching themes:', error);
+      }
+    };
+
+    fetchThemes();
+  }, []);
+
+  const currentTheme = getCurrentTheme(themeKey, Themes);
+  console.log(currentTheme);
 
   if (!currentTheme) {
     return <Navigate to={RouterPath.notFound} />;

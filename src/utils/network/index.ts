f2aba1dba476e7a64432543157fbf093a429622d
cla6shade <cla6shade@gmail.com>
2024-07-09 23:37:32 +0900

    feat: path param을 대체해주는 유틸 함수

--- src/utils/network/index.ts
+++ src/utils/network/index.ts
@@ -2,9 +2,17 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
-// eslint-disable-next-line import/prefer-default-export
-export { axiosInstance };
+type RequestPath = { [key: string]: string };
+
+export function replacePathParams(url: string, pathParams: RequestPath): string {
+  let replacedUrl = url;
+  Object.entries(pathParams).forEach(([key, value]) => {
+    replacedUrl = replacedUrl.replace(`:${key}`, value.toString());
+  });
+
+  return replacedUrl;
+}

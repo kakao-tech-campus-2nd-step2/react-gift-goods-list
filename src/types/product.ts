7581728134f16106a5d14f78b7fe72308f22efe9
박건규 <geongyu09@gmail.com>
2024-07-09 14:43:06 +0900

    feat: api문서에 따라 product 응답 타입을 정의 (product.ts)

---
+++ src/types/product.ts
@@ -0,0 +1,59 @@
+export interface Product {
+  id: number;
+  name: string;
+  imageURL: string;
+  wish: {
+    wishCount: number;
+    isWished: boolean;
+  };
+  price: {
+    basicPrice: number;
+    discountRate: number;
+    sellingPrice: number;
+  };
+  brandInfo: {
+    id: number;
+    name: string;
+    imageURL: string;
+  };
+}
+
+export interface Theme {
+  id: number;
+  key: string;
+  label: string;
+  title: string;
+  description: string;
+  backgroundColor: string;
+}
+
+export interface WishProduct extends Product {
+  nextPageToken: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+}
+
+export interface ProductDetail extends Product {
+  isAccessableProductPage: boolean;
+  review: {
+    averageRating: number;
+    totalReviewCount: number;
+  };
+  productDescription: {
+    images: string[];
+  };
+  productDetailInfo: {
+    announcements: Array<{
+      displayOrder: number;
+      name: string;
+      value: string;
+    }>;
+    terms: Array<{
+      displayOrder: number;
+      title: string;
+      description: string;
+    }>;
+  };
+}

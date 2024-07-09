6c8e72e1aea63de27d96d276f2109d605b8958fb
JaeAnHan <jaean1999@naver.com>
2024-07-09 22:42:33 +0900

    feat: 실시간 급상승 선물랭킹 필터 예시 사이트 참고해서 더 유연하게 동작하도록 수정 -

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,6 +13,21 @@ function createTypeValidation<T extends readonly string[]>(validValues: T) {
 export const isValidTargetType = createTypeValidation(validTargetTypes);
 export const isValidRankType = createTypeValidation(validRankTypes);
 
+export const validTargetTypes = ['ALL', 'FEMALE', 'MALE', 'TEEN'] as const;
+export const validRankTypes = ['MANY_WISH', 'MANY_RECEIVE', 'MANY_WISH_RECEIVE'] as const;
+
+export type TargetType = (typeof validTargetTypes)[number];
+export type RankTypes = (typeof validRankTypes)[number];
+
+function createTypeValidation<T extends readonly string[]>(validValues: T) {
+  return function (value: string): value is T[number] {
+    return (validValues as readonly string[]).includes(value);
+  };
+}
+
+export const isValidTargetType = createTypeValidation(validTargetTypes);
+export const isValidRankType = createTypeValidation(validRankTypes);
+
 export type RankingFilterOption = {
   targetType: TargetType;
   rankType: RankTypes;

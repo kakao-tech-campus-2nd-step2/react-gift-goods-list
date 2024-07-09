9879d54fd381d09be3d8fb2a5c3fc3291f42b489
dandamdandam <glue0440@gmail.com>
2024-07-09 21:14:35 +0900

    feat: get theme data from server

--- src/pages/Main/ThemeList.tsx
+++ src/pages/Main/ThemeList.tsx
@@ -4,42 +4,44 @@ import { Link } from 'react-router-dom';
 import { Image } from '@/components/common/Image';
 import { Container } from '@/components/common/layouts/Container';
 import { Grid } from '@/components/common/layouts/Grid';
-
-import { themeKeys } from '../Theme';
+import type { Themes } from '@/entities/Theme';
+import useGetData from '@/hooks/useGetData';
 
 export default () => {
-    const ThemeButton = ({ themeKey }: { themeKey: string }) => {
-        const divStyle = css`
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            font-size: 15px;
-            img {
-                max-width: 90px;
-                margin-bottom: 8px;
-            }
-            padding: 20px 0px;
-        `;
-        return (
-            <Container>
-                <Link className={divStyle} to={`/theme/${themeKey}`}>
-                    <Image
-                        radius={30}
-                        ratio="square"
-                        alt={themeKey}
-                        src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-                    />
-                    <p>{themeKeys[themeKey].label}</p>
-                </Link>
-            </Container>
-        );
-    };
+    const themes = useGetData<Themes>('/themes');
 
     return (
         <Grid columns={{ initial: 2, xs: 4, sm: 4, md: 6 }}>
-            {Object.keys(themeKeys).map((themeKey) => (
-                <ThemeButton key={themeKey} themeKey={themeKey} />
+            {themes?.themes.map((theme) => (
+                <ThemeButton key={theme.id} themeKey={theme.key} themeLabel={theme.label} themeImg={theme.imageURL} />
             ))}
         </Grid>
+    );
+};
+
+const ThemeButton = ({ themeKey, themeLabel, themeImg }: { themeKey: string, themeLabel: string, themeImg: string }) => {
+    const divStyle = css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-size: 15px;
+        img {
+            max-width: 90px;
+            margin-bottom: 8px;
+        }
+        padding: 20px 0px;
+    `;
+    return (
+        <Container>
+            <Link className={divStyle} to={`/theme/${themeKey}`}>
+                <Image
+                    radius={30}
+                    ratio="square"
+                    alt={themeKey}
+                    src={themeImg}
+                />
+                <p>{themeLabel}</p>
+            </Link>
+        </Container>
     );
 };

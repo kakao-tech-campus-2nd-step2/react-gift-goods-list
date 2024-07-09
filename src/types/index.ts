a7e4002ff57397c1cbafa3d0bfe9d0058452d67f
HyeonChae1104 <211063@jnu.ac.kr>
2024-07-09 15:17:24 +0900

    feat: Request, Response Type 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -31,3 +31,107 @@ export type GoodsData = {
     imageURL: string;
   };
 };
+
+export interface RankingProductsRequest {
+  targetType?: 'ALL' | 'FEMALE' | 'MALE' | 'TEEN';
+  rankType?: 'MANY_WISH' | 'MANY_RECEIVE' | 'MANY_WISH_RECEIVE';
+}
+
+export interface RankingProductsResponse {
+  products: GoodsData[];
+}
+
+export interface ThemesResponse {
+  themes: ThemeData[];
+}
+
+export interface ThemeProductsRequest {
+  themeKey: string;
+  pageToken?: string;
+  maxResults?: number;
+}
+
+export interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+export interface ThemeProductsResponse {
+  products: GoodsData[];
+  nextPageToken: string | null;
+  pageInfo: PageInfo;
+}
+
+export interface ProductDetailRequest {
+  productId: string;
+}
+
+export interface ProductDetailData extends GoodsData {
+  isAccessableProductPage: boolean;
+  review: {
+    averageRating: number;
+    totalReviewCount: number;
+  };
+  productDescription: {
+    images: string[];
+  };
+  productDetailInfo: {
+    announcements: {
+      displayOrder: number;
+      name: string;
+      value: string;
+    }[];
+    terms: {
+      displayOrder: number;
+      title: string;
+      description: string;
+    }[];
+  };
+}
+
+export interface MessageCardTemplateData {
+  id: number;
+  defaultTextMessage: string;
+  thumbURL: string;
+  imageURL: string;
+}
+
+export interface MessageCardTemplatesResponse {
+  templates: MessageCardTemplateData[];
+}
+
+export interface MyAccountInfoData {
+  id: number;
+  name: string;
+  birthday: string;
+  profileImageURL: string;
+  point: number;
+}
+
+export interface MyAccountWishProductsRequest {
+  pageToken?: string;
+  maxResults?: number;
+}
+
+export interface MyAccountWishProductsResponse {
+  products: GoodsData[];
+  nextPageToken: string | null;
+  pageInfo: PageInfo;
+}
+
+export interface MyAccountPointRequest {
+  point: number;
+}
+
+export interface ProductOrderRequestBody {
+  productId: number;
+  productOptionId: number;
+  productQuantity: number;
+  messageCardTemplateId: number;
+  messageCardTextMessage: string;
+  senderId: number;
+  receiverId: number;
+  hasCashReceipt: boolean;
+  cashReceiptType?: 'PERSONAL' | 'BUSINESS';
+  cashReceiptNumber?: string;
+}

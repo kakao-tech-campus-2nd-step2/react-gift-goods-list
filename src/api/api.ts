845cff23e3d6e23a5e03468d7c60619da9df57b0
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-07-09 13:41:10 +0900

    feat: 메인페이지- 실시간 급상승 선물랭킹 섹션 api이용하여 구현

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ThemeData } from '@/types/index';
+import { ProductData, ThemeData } from '@/types/index';
 
 const api = axios.create({
   baseURL: 'https://react-gift-mock-api-hojeong26-git-main-hojeongs-projects.vercel.app',
@@ -8,4 +8,9 @@ const api = axios.create({
 export const fetchThemesFromAPI = async (): Promise<ThemeData[]> => {
   const response = await api.get<{ themes: ThemeData[] }>('/api/v1/themes');
   return response.data.themes;
+};
+
+export const fetchRankingFromAPI = async (): Promise<ProductData[]> => {
+  const response = await api.get<{ products: ProductData[] }>('api/v1/ranking/products');
+  return response.data.products;
 };
--- src/components/features/Home/GoodsRankingSection/index.tsx
+++ src/components/features/Home/GoodsRankingSection/index.tsx
@@ -1,19 +1,41 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Container } from '@/components/common/layouts/Container';
 import { breakpoints } from '@/styles/variants';
-import type { RankingFilterOption } from '@/types';
-import { GoodsMockList } from '@/types/mock';
+import type { ProductData, RankingFilterOption } from '@/types';
+import { fetchRankingFromAPI } from '@/api/api';
+// import { GoodsMockList } from '@/types/mock';
 
 import { GoodsRankingFilter } from './Filter';
 import { GoodsRankingList } from './List';
 
 export const GoodsRankingSection = () => {
+  const [rankProducts, setRankProducts] = useState<ProductData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [fetchError, setFetchError] = useState<Error | null>(null);
+
   const [filterOption, setFilterOption] = useState<RankingFilterOption>({
     targetType: 'ALL',
     rankType: 'MANY_WISH',
   });
+
+  useEffect(() => {
+    const fetchRankingProducts = async () => {
+      try {
+        const fetchedRankProducts = await fetchRankingFromAPI();
+        setRankProducts(fetchedRankProducts);
+      } catch (error) {
+        setFetchError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchRankingProducts();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (fetchError) return <div>Error loading themes</div>;
 
   // GoodsMockData를 21번 반복 생성
 
@@ -22,7 +44,7 @@ export const GoodsRankingSection = () => {
       <Container>
         <Title>실시간 급상승 선물랭킹</Title>
         <GoodsRankingFilter filterOption={filterOption} onFilterOptionChange={setFilterOption} />
-        <GoodsRankingList goodsList={GoodsMockList} />
+        <GoodsRankingList goodsList={rankProducts} />
       </Container>
     </Wrapper>
   );

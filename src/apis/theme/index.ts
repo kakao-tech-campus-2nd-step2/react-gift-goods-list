db3517b1cd3996647b05b208774939854b6a1df9
박건규 <geongyu09@gmail.com>
2024-07-09 15:13:18 +0900

    feat: theme api 구현

--- src/apis/theme/index.ts
+++ src/apis/theme/index.ts
@@ -1,9 +1,9 @@
-import type { Theme } from '@/types/product';
+import type { ThemeData } from '@/types';
 
 import instance from '../instance';
 import API from '../path';
 
-export const getThemes = async (): Promise<Theme[]> => {
-  const res = await instance.get(API.THEMES);
-  return res.data!.themes;
+export const getThemes = async (): Promise<ThemeData[]> => {
+  const res = await instance.get<ThemeData[]>(API.THEMES);
+  return res.data;
 };

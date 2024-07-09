c76ea7b112f1c2afb44a54a18a0f50d359b394c0
Dobbymin <gmin0701@knu.ac.kr>
2024-07-09 22:08:27 +0900

    feat(theme): 테마 선택 버튼 data 불러오기 기능 구현중

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,14 +1,44 @@
+import { useEffect, useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
+import { getThemes } from '@/apis/Theme/theme.api';
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
-import { getCurrentTheme, ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
+import {
+  getCurrentTheme,
+  ThemeHeroSection,
+} from '@/components/features/Theme/ThemeHeroSection';
 import { RouterPath } from '@/routes/path';
-import { ThemeMockList } from '@/types/mock';
+
+// import { ThemeMockList } from '@/types/mock';
+
+interface ThemeItem {
+  id: number;
+  key: string;
+  label: string;
+  imageURL: string;
+  title: string;
+  description?: string;
+  backgroundColor: string;
+}
 
 export const ThemePage = () => {
   const { themeKey = '' } = useParams<{ themeKey: string }>();
-  const currentTheme = getCurrentTheme(themeKey, ThemeMockList);
+  const [themeData, setThemeData] = useState<ThemeItem[]>([]);
 
+  useEffect(() => {
+    const fetchThemes = async () => {
+      const data = await getThemes();
+      setThemeData(data.themes);
+    };
+    fetchThemes();
+  }, []);
+
+  // const currentTheme = getCurrentTheme(themeKey, ThemeMockList);
+  const currentTheme = getCurrentTheme(themeKey, themeData);
+
+  console.log('themeData: ', themeData);
+  console.log('currentTheme:', currentTheme);
+  console.log('themeKey:', themeKey);
   if (!currentTheme) {
     return <Navigate to={RouterPath.notFound} />;
   }
@@ -20,3 +50,6 @@ export const ThemePage = () => {
     </>
   );
 };
+// function useState<T>(arg0: never[]): [any, any] {
+//   throw new Error('Function not implemented.');
+// }

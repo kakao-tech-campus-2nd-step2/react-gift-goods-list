bf274cc4652b19d38c19331d419f6010079d7534
박건규 <geongyu09@gmail.com>
2024-07-09 15:56:45 +0900

    fix: 잘못된 타입으로 정의된 값들 수정

--- src/apis/theme/index.ts
+++ src/apis/theme/index.ts
@@ -1,9 +1,10 @@
-import type { ThemeData } from '@/types';
+import type { Theme } from '@/types/product';
 
 import instance from '../instance';
 import API from '../path';
 
-export const getThemes = async (): Promise<ThemeData[]> => {
-  const res = await instance.get<ThemeData[]>(API.THEMES);
-  return res.data;
+export const getThemes = async (): Promise<Theme[]> => {
+  const res = await instance.get(API.THEMES);
+  console.log(res.data.themes);
+  return res.data!.themes;
 };

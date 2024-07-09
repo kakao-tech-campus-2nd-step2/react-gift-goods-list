82da35f93a5bc29001d300dc6075ca54af13bf20
anheejeong <eyrt6973@naver.com>
2024-07-09 18:10:17 +0900

    [Feat] Response Type 지정

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,10 @@
-export type ThemeData = {
+
+
+export type ThemeData = { // /api/v1/themes
   id: number;
   key: string;
   label: string;
+  imageUrl: string;
   title: string;
   description?: string;
   backgroundColor: string;
@@ -12,18 +15,18 @@ export type RankingFilterOption = {
   rankType: 'MANY_WISH' | 'MANY_RECEIVE' | 'MANY_WISH_RECEIVE';
 };
 
-export type GoodsData = {
+export type GoodsData = { // /api/v1/ranking/products && /api/v1/themes/{themeKey}/products
   id: number;
   name: string;
   imageURL: string;
   wish: {
-    wishCount: number;
     isWished: boolean;
+    wishCount: number;
   };
   price: {
     basicPrice: number;
-    discountRate: number;
     sellingPrice: number;
+    discountRate: number;
   };
   brandInfo: {
     id: number;
@@ -31,3 +34,8 @@ export type GoodsData = {
     imageURL: string;
   };
 };
+
+
+export type ProductsData = {
+
+}

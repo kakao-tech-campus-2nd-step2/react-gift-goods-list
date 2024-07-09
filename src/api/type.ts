d10b5ff8443676e4d0e137b9ad82e15b749d69c5
Eric <real.dassy@gmail.com>
2024-07-09 15:40:33 +0900

    feat: ProductData 인터페이스 추가
    API 응답 시 데이터의 형식인 ProductData 인터페이스를 추가합니다.

--- src/api/type.ts
+++ src/api/type.ts
@@ -2,3 +2,23 @@ export interface GetRankingProductsParameters {
   targetType?: 'ALL' | 'FEMALE' | 'MALE' | 'TEEN';
   rankType?: 'MANY_WISH' | 'MANY_RECEIVE' | 'MANY_WISH_RECEIVE';
 }
+
+export interface ProductData {
+  id: number;
+  name: string;
+  imageURL: string;
+  wish: {
+    wishCount: number;
+    isWished: boolean;
+  };
+  price: {
+    basicPrice: number;
+    discountRate: number;
+    sellingPrice: number;
+  };
+  brandInfo: {
+    id: number;
+    name: string;
+    imageURL: string;
+  };
+}

6a56039cc611e04643cdf3562f344d80761cb840
minji2219 <200146@jnu.ac.kr>
2024-07-09 16:49:41 +0900

    Feat: FetchState type 공통 타입으로 만들기

--- src/components/features/Theme/ThemeGoodsSection/index.tsx
+++ src/components/features/Theme/ThemeGoodsSection/index.tsx
@@ -2,21 +2,18 @@ import styled from '@emotion/styled';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+import { Error } from '@/components/common/Error';
 import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
 import { Container } from '@/components/common/layouts/Container';
 import { Grid } from '@/components/common/layouts/Grid';
 import { LoadingIcon } from '@/components/common/LoadingIcon';
 import { breakpoints } from '@/styles/variants';
-import type { GoodsData } from '@/types';
+import type { FetchState, GoodsData } from '@/types';
 
 type Props = {
   themeKey: string;
 };
-interface FetchState<T> {
-  isLoading: boolean;
-  isError: boolean;
-  data: T | null;
-}
+
 export const ThemeGoodsSection = ({ themeKey }: Props) => {
   const [fetchState, setFetchState] = useState<FetchState<GoodsData[]>>({
     isLoading: true,
@@ -76,8 +73,4 @@ const Wrapper = styled.section`
   @media screen and (min-width: ${breakpoints.sm}) {
     padding: 40px 16px 360px;
   }
-`;
-const Error = styled.div`
-  margin: 50px 0;
-  text-align: center;
 `;

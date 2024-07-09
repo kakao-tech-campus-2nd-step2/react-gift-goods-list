b14fc6cd64c778424934d589a06480b5483833e7
dlawlghks <ikuwlghks@naver.com>
2024-07-09 19:03:02 +0900

    fix: ThemePage import 수정 및 useCurrentTheme 훅 적용 수정

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,13 +1,15 @@
 import { Navigate, useParams } from 'react-router-dom';
 
+import { useCurrentTheme } from '@/api/hooks/useCurrentTheme';
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
-import { getCurrentTheme, ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
+import { ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
 import { RouterPath } from '@/routes/path';
-import { ThemeMockList } from '@/types/mock';
 
 export const ThemePage = () => {
   const { themeKey = '' } = useParams<{ themeKey: string }>();
-  const currentTheme = getCurrentTheme(themeKey, ThemeMockList);
+  const { isRender, currentTheme } = useCurrentTheme({ themeKey });
+
+  if (!isRender) return null;
 
   if (!currentTheme) {
     return <Navigate to={RouterPath.notFound} />;

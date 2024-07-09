81ce3406cd972d8f56404aa6b5eb21c327338b53
Eric <real.dassy@gmail.com>
2024-07-09 15:43:57 +0900

    feat: ProductData 내부 객체 인터페이스 추가
    ProductData 내부에서 사용되는 객체인 Wish, Price, BrandInfo 인터페이스를 추가합니다.

--- src/api/type.ts
+++ src/api/type.ts
@@ -3,22 +3,28 @@ export interface GetRankingProductsParameters {
   rankType?: 'MANY_WISH' | 'MANY_RECEIVE' | 'MANY_WISH_RECEIVE';
 }
 
+export interface Wish {
+  wishCount: number;
+  isWished: boolean;
+}
+
+export interface Price {
+  baseicPrice: number;
+  discountRate: number;
+  sellingPrice: number;
+}
+
+export interface BrandInfo {
+  id: number;
+  name: string;
+  imageURL: string;
+}
+
 export interface ProductData {
   id: number;
   name: string;
   imageURL: string;
-  wish: {
-    wishCount: number;
-    isWished: boolean;
-  };
-  price: {
-    basicPrice: number;
-    discountRate: number;
-    sellingPrice: number;
-  };
-  brandInfo: {
-    id: number;
-    name: string;
-    imageURL: string;
-  };
+  wish: Wish;
+  price: Price;
+  brandInfo: BrandInfo;
 }

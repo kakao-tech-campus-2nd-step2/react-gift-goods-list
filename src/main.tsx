ed854c0d0bb227f88770d7cd1b104b774d8099ef
cla6shade <cla6shade@gmail.com>
2024-07-09 22:49:27 +0900

    feat: 테마 컨텍스트 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import router from '@router';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from '@styles/GlobalStyle';
 import LoginContextProvider from '@/providers/LoginContextProvider';
+import ThemeContextProvider from '@/providers/ThemeContextProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyle />
     <LoginContextProvider>
-      <RouterProvider router={router} />
+      <ThemeContextProvider>
+        <RouterProvider router={router} />
+      </ThemeContextProvider>
     </LoginContextProvider>
   </React.StrictMode>,
 );

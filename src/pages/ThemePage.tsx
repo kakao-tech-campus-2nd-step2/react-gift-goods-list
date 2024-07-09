2cf3b375715e602fd4e4906a047f9dbbc67c8049
cla6shade <cla6shade@gmail.com>
2024-07-09 23:17:31 +0900

    feat: 테마 페이지에서 실제 데이터를 사용하도록 구현

--- src/pages/ThemePage.tsx
+++ src/pages/ThemePage.tsx
@@ -1,27 +1,31 @@
-import { useParams } from 'react-router-dom';
-import Themes from '@constants/Themes';
+import { useNavigate, useParams } from 'react-router-dom';
 import Page from '@components/templates/Page';
 import Banner from '@components/organisms/banner/Banner';
 import GiftDisplaySection from '@components/organisms/gift/GiftDisplaySection';
 import useFetchProducts from '@hooks/useFetchProducts';
 import Container from '@components/atoms/container/Container';
 import { MAX_CONTENT_WIDTH } from '@styles/size';
+import { useContext, useEffect } from 'react';
+import { ThemeContext } from '@/providers/ThemeContextProvider';
+import { isThemesLoaded } from '@/utils';
 
 function ThemePage() {
   const { themeKey } = useParams();
-  const gifts = useFetchProducts({ themeFilter: themeKey });
+  const products = useFetchProducts({});
+  const themes = useContext(ThemeContext);
+  const navigate = useNavigate();
 
-  if (!themeKey || !(themeKey in Themes)) {
-    return (
-      <div>
-        테마 지정 오류!
-      </div>
-    );
-  }
+  useEffect(() => {
+    if (!isThemesLoaded(themes)) return;
 
-  return (
+    if (!themeKey || !(themeKey in themes)) {
+      navigate(-1);
+    }
+  }, [products, navigate, themes, themeKey]);
+
+  return isThemesLoaded(themes) ? (
     <Page>
-      <Banner themeKey={themeKey} />
+      <Banner themeKey={themeKey as string} />
       <Container elementSize="full-width" justifyContent="center">
         <Container
           elementSize="full-width"
@@ -29,11 +33,11 @@ function ThemePage() {
           justifyContent="center"
           padding="40px 16px 300px"
         >
-          <GiftDisplaySection gifts={gifts} maxColumns={4} minColumns={2} />
+          <GiftDisplaySection products={products} maxColumns={4} minColumns={2} />
         </Container>
       </Container>
     </Page>
-  );
+  ) : null;
 }
 
 export default ThemePage;

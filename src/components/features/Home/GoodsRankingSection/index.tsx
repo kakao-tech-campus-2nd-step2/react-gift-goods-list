3b60d0e0666da3153f1ec86ba5ac767c275ee09c
wjdsbs <wjdsbss@gmail.com>
2024-07-10 23:13:29 +0900

    feat: 실시간 급상승 선물 랭킹 Error, Loading Status 핸들링 기능 추가

--- src/components/features/Home/GoodsRankingSection/index.tsx
+++ src/components/features/Home/GoodsRankingSection/index.tsx
@@ -9,16 +9,27 @@ import type { GoodsData, RankingFilterOption } from '@/types';
 import { GoodsRankingFilter } from './Filter';
 import { GoodsRankingList } from './List';
 
+interface FetchState<T> {
+  isLoading: boolean;
+  isError: boolean;
+  data: T | null;
+}
+
 export const GoodsRankingSection = () => {
   const [filterOption, setFilterOption] = useState<RankingFilterOption>({
     targetType: 'ALL',
     rankType: 'MANY_WISH',
   });
 
-  const [goodsList, setGoodsList] = useState<GoodsData[]>([]);
+  const [fetchState, setFetchState] = useState<FetchState<GoodsData[]>>({
+    isLoading: true,
+    isError: false,
+    data: null,
+  });
 
   useEffect(() => {
     const fetchRanking = async () => {
+      setFetchState({ isLoading: true, isError: false, data: [] });
       try {
         const response = await axiosInstance.get('/api/v1/ranking/products', {
           params: {
@@ -29,21 +40,32 @@ export const GoodsRankingSection = () => {
 
         const { products } = response.data;
 
-        setGoodsList(products);
+        setFetchState({ isLoading: false, isError: false, data: products });
       } catch (error) {
         console.error('Error fetching ranking:', error);
+        setFetchState({ isLoading: false, isError: true, data: null });
       }
     };
 
     fetchRanking();
   }, [filterOption]);
 
+  const { isLoading, isError, data } = fetchState;
+
   return (
     <Wrapper>
       <Container>
         <Title>실시간 급상승 선물랭킹</Title>
         <GoodsRankingFilter filterOption={filterOption} onFilterOptionChange={setFilterOption} />
-        <GoodsRankingList goodsList={goodsList} />
+        {isLoading ? (
+          <Description>로딩 중</Description>
+        ) : isError ? (
+          <Description>데이터를 불러오는 중에 문제가 발생했습니다.</Description>
+        ) : data && data.length > 0 ? (
+          <GoodsRankingList goodsList={data} />
+        ) : (
+          <Description>보여줄 상품이 없어요!</Description>
+        )}
       </Container>
     </Wrapper>
   );
@@ -70,4 +92,15 @@ const Title = styled.h2`
     font-size: 35px;
     line-height: 50px;
   }
+`;
+
+const Description = styled.div`
+  width: 100%;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  padding: 40px 16px 60px;
+  font-size: 16px;
 `;

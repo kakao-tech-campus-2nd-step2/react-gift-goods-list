ac769ab897f6c31369e09f0e369f413ae48f4b4b
YIMSEBIN <jij09123@gmail.com>
2024-07-10 16:50:58 +0900

    feat: theme 상세페이지로 이동 API 연결

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
 import { getCurrentTheme, ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
@@ -11,6 +11,7 @@ export const ThemePage = () => {
   const { themeKey = '' } = useParams<{ themeKey: string }>();
   const [themes, setThemes] = useState<ThemeData[]>([]);
   const [currentTheme, setCurrentTheme] = useState<ThemeData>();
+  const [loading, setLoading] = useState(true);
 
   const url = 'https://react-gift-mock-api-two.vercel.app/api/v1/themes';
   useEffect(() => {
@@ -19,19 +20,19 @@ export const ThemePage = () => {
       // console.log(res.data.themes); // API로부터 받은 실제 데이터 출력
     });
   }, []); // 의존성 배열을 빈 배열로 설정하여 마운트 시에만 실행되도록 함
-  console.log(themes); // 첫 렌더링에서는 빈 배열, 업데이트 후에는 API 데이터를 출력
-
-  // 아래 방식으로 themes를 확인해봐도 빈 배열이 뜬다..
-  useEffect(() => {
-    console.log(themes);
-  }, [themes]);
+  // console.log(themes); // 첫 렌더링에서는 빈 배열, 업데이트 후에는 API 데이터를 출력
 
   useEffect(() => {
     setCurrentTheme(getCurrentTheme(themeKey, themes));
   }, [themeKey, themes]);
 
-  if (!currentTheme) {
-    return <Navigate to={RouterPath.notFound} />;
+  useEffect(() => {
+    console.log(currentTheme);
+    setLoading(false);
+  }, [currentTheme]);
+
+  if (!loading) {
+    if (!currentTheme) return <Link to={RouterPath.notFound} />;
   }
   return (
     <>

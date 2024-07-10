5f90d62502fa35d59ab2aa83fcc50b4085ca88a1
KimJi-An <j0012a@naver.com>
2024-07-11 00:31:48 +0900

    feat: Theme 페이지의 상품 목록 섹션 Loading 상태에 대한 UI 대응 및 기본 에러 처리

--- src/components/features/Theme/ThemeGoodsSection/index.tsx
+++ src/components/features/Theme/ThemeGoodsSection/index.tsx
@@ -5,6 +5,7 @@ import { fetchThemeProducts } from '@/api/themeProducts';
 import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
 import { Container } from '@/components/common/layouts/Container';
 import { Grid } from '@/components/common/layouts/Grid';
+import { Message } from '@/components/common/Message';
 import { breakpoints } from '@/styles/variants';
 import type { ProductData } from '@/types/api';
 
@@ -15,10 +16,22 @@ type Props = {
 export const ThemeGoodsSection = ({ themeKey }: Props) => {
   const [products, setProducts] = useState<ProductData[]>([]);
 
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
+    setIsError(false);
+
     const getProducts = async () => {
-      const productsData = await fetchThemeProducts(themeKey);
-      setProducts(productsData);
+      try {
+        const productsData = await fetchThemeProducts(themeKey);
+        setProducts(productsData);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setLoading(false);
+      }
     };
 
     getProducts();
@@ -27,6 +40,9 @@ export const ThemeGoodsSection = ({ themeKey }: Props) => {
   return (
     <Wrapper>
       <Container>
+      {loading && <LoadingContainer><Loading /></LoadingContainer>}
+        {!loading && isError && <Message>데이터를 불러오는 중에 문제가 발생했습니다.</Message>}
+        {!loading && !isError && products.length === 0 && <Container><Message>보여줄 상품이 없어요!</Message></Container>}
         <Grid
           columns={{
             initial: 2,
@@ -55,5 +71,27 @@ const Wrapper = styled.section`
 
   @media screen and (min-width: ${breakpoints.sm}) {
     padding: 40px 16px 360px;
+  }
+`;
+
+const LoadingContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loading = styled.div`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-left-color: rgb(255, 255, 255);
+  animation: spin 1s linear infinite;
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
   }
 `;

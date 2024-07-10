d1ff12db9e9b9746a73915cd6634b6f6d8f057f3
HyeonChae1104 <211063@jnu.ac.kr>
2024-07-10 16:46:46 +0900

    chore: 오류 수정

--- src/components/features/Theme/Header/index.tsx
+++ src/components/features/Theme/Header/index.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate,useParams } from 'react-router-dom';
 
 import { fetchThemes } from '@/api/themes';

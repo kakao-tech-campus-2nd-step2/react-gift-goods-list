3ebd8d98648c8804ec22c0686a7ec51bc7fdd5d9
JeongSeoyun <xeonj@naver.com>
2024-07-10 14:36:42 +0900

    feat: 메인페이지 실시간 급상승 선물랭킹 섹션 api 구현

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://react-gift-mock-api-pearl.vercel.app/',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'https://react-gift-mock-api-pearl.vercel.app/'
 });
 
 export default axiosInstance;
--- src/components/features/Home/GoodsRankingSection/index.tsx
+++ src/components/features/Home/GoodsRankingSection/index.tsx
@@ -1,11 +1,13 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+import type { ProductData } from '@/api/api';
+import axiosInstance from '@/api/axiosInstance';
 import { Container } from '@/components/common/layouts/Container';
 import { breakpoints } from '@/styles/variants';
 import type { RankingFilterOption } from '@/types';
-import { GoodsMockList } from '@/types/mock';
 
+//import { GoodsMockList } from '@/types/mock';
 import { GoodsRankingFilter } from './Filter';
 import { GoodsRankingList } from './List';
 
@@ -17,12 +19,44 @@ export const GoodsRankingSection = () => {
 
   // GoodsMockData를 21번 반복 생성
 
+  const [goodsList, setGoodsList] = useState<ProductData[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  const fetchRankingProducts = async (filters: RankingFilterOption) => {
+    setLoading(true)
+    setError(null)
+    try {
+      const response = await axiosInstance.get('/api/v1/ranking/products', { params: filters })
+      setGoodsList(response.data.products)
+    } catch (err) {
+      console.error(err)
+      setError('ranking products를 fetch하는데 실패함')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchRankingProducts(filterOption)
+  }, [filterOption])
+
+  if (loading)
+    return (
+      <div>Loading...</div>
+    )
+  
+  if (error)
+    return (
+      <div>{error}</div>
+  )
+
   return (
     <Wrapper>
       <Container>
         <Title>실시간 급상승 선물랭킹</Title>
         <GoodsRankingFilter filterOption={filterOption} onFilterOptionChange={setFilterOption} />
-        <GoodsRankingList goodsList={GoodsMockList} />
+        <GoodsRankingList goodsList={goodsList} />
       </Container>
     </Wrapper>
   );

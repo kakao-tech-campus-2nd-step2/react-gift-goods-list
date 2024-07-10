0f0a7e3622985ec768ab538e068821356f5a4949
KimKyuHoi <k546kh@gmail.com>
2024-07-10 18:25:33 +0900

    Feat: Infinity 스크롤 구현

--- src/components/features/Theme/ThemeGoodsSection/index.tsx
+++ src/components/features/Theme/ThemeGoodsSection/index.tsx
@@ -1,16 +1,53 @@
 import styled from '@emotion/styled';
+// import { GoodsMockList } from '@/types/mock';
+import { useCallback, useEffect, useRef } from 'react';
+import { Spinner } from 'react-bootstrap';
 
+import { useGetThemeProductQuery } from '@/apis/tanstackQuery/theme/query';
 import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
 import { Container } from '@/components/common/layouts/Container';
 import { Grid } from '@/components/common/layouts/Grid';
 import { breakpoints } from '@/styles/variants';
-import { GoodsMockList } from '@/types/mock';
 
 type Props = {
   themeKey: string;
 };
 
-export const ThemeGoodsSection = ({}: Props) => {
+export const ThemeGoodsSection = ({ themeKey }: Props) => {
+  const { products, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage, error } =
+    useGetThemeProductQuery(themeKey);
+
+  const observerTarget = useRef(null);
+
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      const [target] = entries;
+      if (target.isIntersecting && hasNextPage && !isFetchingNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage, isFetchingNextPage],
+  );
+
+  useEffect(() => {
+    const currentObserverTarget = observerTarget.current;
+    const option = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 0,
+    };
+
+    const observer = new IntersectionObserver(handleObserver, option);
+
+    if (currentObserverTarget) observer.observe(currentObserverTarget);
+
+    return () => {
+      if (currentObserverTarget) observer.unobserve(currentObserverTarget);
+    };
+  }, [handleObserver]);
+
+  if (isError) return <LoadingMessage>{error?.message}</LoadingMessage>;
+
   return (
     <Wrapper>
       <Container>
@@ -21,7 +58,7 @@ export const ThemeGoodsSection = ({}: Props) => {
           }}
           gap={16}
         >
-          {GoodsMockList.map(({ id, imageURL, name, price, brandInfo }) => (
+          {products.map(({ id, imageURL, name, price, brandInfo }) => (
             <DefaultGoodsItems
               key={id}
               imageSrc={imageURL}
@@ -31,6 +68,14 @@ export const ThemeGoodsSection = ({}: Props) => {
             />
           ))}
         </Grid>
+        {(isLoading || isFetchingNextPage) && (
+          <LoadingMessage>
+            <Spinner animation="border" role="status" variant="secondary">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </LoadingMessage>
+        )}
+        <div ref={observerTarget} />
       </Container>
     </Wrapper>
   );
@@ -43,4 +88,10 @@ const Wrapper = styled.section`
   @media screen and (min-width: ${breakpoints.sm}) {
     padding: 40px 16px 360px;
   }
+`;
+
+const LoadingMessage = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;

1f9d42f794ede2152ab24282502ed20deba2cfc4
dlawlghks <ikuwlghks@naver.com>
2024-07-10 22:58:40 +0900

    refactor: isRender 상태 제거 및 currentTheme 존재 여부로 처리

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,4 +1,6 @@
+import styled from '@emotion/styled';
 import { Navigate, useParams } from 'react-router-dom';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 import { useCurrentTheme } from '@/api/hooks/useCurrentTheme';
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
@@ -9,7 +11,14 @@ export const ThemePage = () => {
   const { themeKey = '' } = useParams<{ themeKey: string }>();
   const { isLoading, currentTheme } = useCurrentTheme({ themeKey });
 
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) {
+    return (
+      <LoadingContainer>
+        <ClipLoader color="#36d7b7" loading={isLoading} size={50} />
+        <LoadingText>Loading...</LoadingText>
+      </LoadingContainer>
+    );
+  }
 
   if (!currentTheme) {
     return <Navigate to={RouterPath.notFound} />;
@@ -22,3 +31,18 @@ export const ThemePage = () => {
     </>
   );
 };
+
+const LoadingContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 40px 16px 60px;
+`;
+
+const LoadingText = styled.p`
+  margin-top: 10px;
+  font-size: 16px;
+  color: #36d7b7;
+`;

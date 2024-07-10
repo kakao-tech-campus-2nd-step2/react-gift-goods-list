192d5a3925c9b3f18c001d1a277ad259bcd4ccad
wjdsbs <wjdsbss@gmail.com>
2024-07-10 18:45:58 +0900

    feat: 메인페이지 - 실시간 급상승 선물랭킹 섹션 구현

--- src/components/features/Home/GoodsRankingSection/index.tsx
+++ src/components/features/Home/GoodsRankingSection/index.tsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+import { axiosInstance } from '@/api';
 import { Container } from '@/components/common/layouts/Container';
 import { breakpoints } from '@/styles/variants';
-import type { RankingFilterOption } from '@/types';
-import { GoodsMockList } from '@/types/mock';
+import type { GoodsData, RankingFilterOption } from '@/types';
 
 import { GoodsRankingFilter } from './Filter';
 import { GoodsRankingList } from './List';
@@ -15,14 +15,35 @@ export const GoodsRankingSection = () => {
     rankType: 'MANY_WISH',
   });
 
-  // GoodsMockData를 21번 반복 생성
+  const [goodsList, setGoodsList] = useState<GoodsData[]>([]);
+
+  useEffect(() => {
+    const fetchRanking = async () => {
+      try {
+        const response = await axiosInstance.get('/api/v1/ranking/products', {
+          params: {
+            targetType: filterOption.targetType,
+            rankType: filterOption.rankType,
+          },
+        });
+
+        const { products } = response.data;
+
+        setGoodsList(products);
+      } catch (error) {
+        console.error('Error fetching ranking:', error);
+      }
+    };
+
+    fetchRanking();
+  }, [filterOption]);
 
   return (
     <Wrapper>
       <Container>
         <Title>실시간 급상승 선물랭킹</Title>
         <GoodsRankingFilter filterOption={filterOption} onFilterOptionChange={setFilterOption} />
-        <GoodsRankingList goodsList={GoodsMockList} />
+        <GoodsRankingList goodsList={goodsList} />
       </Container>
     </Wrapper>
   );

3eb48cd22cf39366fabf115471e620633181b787
박건규 <geongyu09@gmail.com>
2024-07-10 15:09:44 +0900

    feat: 에러 타입을 unknown에서 Error 로 변경

--- src/hooks/useQueryState/index.ts
+++ src/hooks/useQueryState/index.ts
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export default function useQueryState<T>() {
   const [data, setData] = useState<T>();
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<unknown>();
+  const [error, setError] = useState<Error>();
 
   return {
     data,

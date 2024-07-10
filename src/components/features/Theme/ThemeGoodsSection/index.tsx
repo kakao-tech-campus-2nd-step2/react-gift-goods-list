99b7db4dae64efba6ba977b0055354f253fb875c
wjdsbs <wjdsbss@gmail.com>
2024-07-10 23:39:19 +0900

    feat: Theme 페이지 - 상품 목록 섹션 Error, Loading Status 핸들링 기능 추가

--- src/components/features/Theme/ThemeGoodsSection/index.tsx
+++ src/components/features/Theme/ThemeGoodsSection/index.tsx
@@ -12,49 +12,71 @@ type Props = {
   themeKey: string;
 };
 
+interface FetchState<T> {
+  isLoading: boolean;
+  isError: boolean;
+  data: T | null;
+}
+
 export const ThemeGoodsSection = ({ themeKey }: Props) => {
-  const [goodsList, setGoodsList] = useState<GoodsData[]>([]);
+  // const [goodsList, setGoodsList] = useState<GoodsData[]>([]);
+  const [fetchState, setFetchState] = useState<FetchState<GoodsData[]>>({
+    isLoading: true,
+    isError: false,
+    data: null,
+  });
 
   useEffect(() => {
     const fetchThemeGoods = async () => {
+      setFetchState({ isLoading: true, isError: false, data: [] });
       try {
         const response = await axiosInstance.get(`/api/v1/themes/${themeKey}/products`, {
           params: {
             maxResults: 20,
           },
         });
-        if (response.data.products) {
-          setGoodsList(response.data.products);
-        }
+        const { products } = response.data;
+        setFetchState({ isLoading: false, isError: false, data: products });
       } catch (error) {
         console.error('Error fetching theme goods:', error);
+        setFetchState({ isLoading: false, isError: true, data: null });
       }
     };
 
     fetchThemeGoods();
   }, [themeKey]);
 
+  const { isLoading, isError, data } = fetchState;
+
   return (
     <Wrapper>
-      <Container>
-        <Grid
-          columns={{
-            initial: 2,
-            md: 4,
-          }}
-          gap={16}
-        >
-          {goodsList.map(({ id, imageURL, name, price, brandInfo }) => (
-            <DefaultGoodsItems
-              key={id}
-              imageSrc={imageURL}
-              title={name}
-              amount={price.sellingPrice}
-              subtitle={brandInfo.name}
-            />
-          ))}
-        </Grid>
-      </Container>
+      {isLoading ? (
+        <Description>로딩 중</Description>
+      ) : isError ? (
+        <Description>에러가 발생했습니다.</Description>
+      ) : data && data.length > 0 ? (
+        <Container>
+          <Grid
+            columns={{
+              initial: 2,
+              md: 4,
+            }}
+            gap={16}
+          >
+            {data.map(({ id, imageURL, name, price, brandInfo }) => (
+              <DefaultGoodsItems
+                key={id}
+                imageSrc={imageURL}
+                title={name}
+                amount={price.sellingPrice}
+                subtitle={brandInfo.name}
+              />
+            ))}
+          </Grid>
+        </Container>
+      ) : (
+        <Description>상품이 없어요.</Description>
+      )}
     </Wrapper>
   );
 };
@@ -66,4 +88,15 @@ const Wrapper = styled.section`
   @media screen and (min-width: ${breakpoints.sm}) {
     padding: 40px 16px 360px;
   }
+`;
+
+const Description = styled.div`
+  width: 100%;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  padding: 40px 16px 60px;
+  font-size: 16px;
 `;

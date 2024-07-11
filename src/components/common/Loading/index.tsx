9f1dd6d6660386679ab7d08f965fa72cdbb8af23
anheejeong <eyrt6973@naver.com>
2024-07-11 15:04:51 +0900

    [Feat] 로딩 컴포넌트 구현

---
+++ src/components/common/Loading/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
+const LoadingSpinner: React.FC = () => {
+    return <Spinner />;
+};
+
+export default LoadingSpinner;
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 5px solid #ccc;
+  border-top: 5px solid #3498db;
+  border-radius: 50%;
+  animation: ${spin} 1s linear infinite;
+`;

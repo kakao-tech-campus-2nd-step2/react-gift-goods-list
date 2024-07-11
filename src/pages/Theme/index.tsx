35f93b5ac12adc680a06af6cd33d3899f3f3f40a
mingkyeongg <ming0820@naver.com>
2024-07-11 14:56:47 +0900

    feat: add loading screen at Theme/index.tsx

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -5,11 +6,11 @@ import apiClient from '@/api';
 import type { GetThemesResponse, ThemeData } from '@/api/types/apiTypes';
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
 import { ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
-// import { RouterPath } from '@/routes/path';
 
 export const ThemePage = () => {
   const { themeKey = '' } = useParams<{ themeKey: string }>();
   const [themes, setThemes] = useState<ThemeData[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchThemes = async () => {
       try {
@@ -17,10 +18,16 @@ export const ThemePage = () => {
         setThemes(response.data.themes);
       } catch (error) {
         console.error(error);
+      } finally { // 오류가 발생하더라도 loading을 false로 변경해야함
+      setLoading(false);
       }
     };
     fetchThemes();
   }, []);
+
+  if (loading) {
+    return <LoadingScreen>Loading...</LoadingScreen>;
+  }
 
   return (
     <>
@@ -29,3 +36,10 @@ export const ThemePage = () => {
     </>
   );
 };
+
+const LoadingScreen = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;

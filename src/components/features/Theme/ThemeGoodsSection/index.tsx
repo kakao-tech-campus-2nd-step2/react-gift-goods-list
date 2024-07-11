37fb498218008e2e4efa06142e383beec0d204e8
Eric <real.dassy@gmail.com>
2024-07-11 23:35:32 +0900

    feat: 테마 상품 목록 에러 및 로딩 상태 처리 로직 추가

--- src/components/features/Theme/ThemeGoodsSection/index.tsx
+++ src/components/features/Theme/ThemeGoodsSection/index.tsx
@@ -3,10 +3,14 @@ import { useEffect, useState } from 'react';
 
 import { ApiService } from '@/api';
 import type { GetThemeProductsParameters, ProductData } from '@/api/types';
+import type { APIError } from '@/api/types';
 import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
 import { Container } from '@/components/common/layouts/Container';
 import { Grid } from '@/components/common/layouts/Grid';
+import { ErrorMessage } from '@/components/features/Error/ErrorMessage';
+import { Loading } from '@/components/features/Loading/Loading';
 import { breakpoints } from '@/styles/variants';
+import { handleApiError } from '@/utils/errorHandler/errorHandler';
 
 type Props = {
   themeKey: string;
@@ -14,20 +18,36 @@ type Props = {
 
 export const ThemeGoodsSection = ({ themeKey }: Props) => {
   const [goodsList, setGoodsList] = useState<ProductData[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const params: GetThemeProductsParameters = { themeKey, maxResults: 21 };
+    const params: GetThemeProductsParameters = { themeKey, maxResults: 20 };
     const fetchGoodsList = async () => {
+      setIsLoading(true);
+
       try {
         const response = await ApiService.fetchThemeProducts(params);
         setGoodsList(response.products);
       } catch (error) {
-        console.error('에러가 발생했습니다.', error);
+        if (error as APIError) {
+          setErrorMessage(handleApiError(error as APIError));
+        }
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchGoodsList();
   }, [themeKey]);
+
+  if (errorMessage) {
+    return <ErrorMessage message={errorMessage} />;
+  }
+
+  if (isLoading) {
+    return <Loading message="로딩 중" />;
+  }
 
   return (
     <Wrapper>

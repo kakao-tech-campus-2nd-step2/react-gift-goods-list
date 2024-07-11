2982a438ba9654dad0f9f744782739a4ab110e4d
JaeAnHan <jaean1999@naver.com>
2024-07-11 23:03:53 +0900

    refactor: 스크롤을 통한 패치 로직 고도화

--- src/hooks/useGoodsSectionControl.ts
+++ src/hooks/useGoodsSectionControl.ts
@@ -1,6 +1,48 @@
+import type { AxiosError } from 'axios';
 import { useEffect, useRef, useState } from 'react';
 
 import { getThemeProducts } from '@/apis/themes/themes';
+
+type LoadingState = {
+  isInit: boolean;
+  loadingMore: boolean;
+};
+
+const defaultLoadState = {
+  isInit: true,
+  loadingMore: false,
+};
+
+const MAX_RETRY_COUNT = 5;
+const RETRY_DELAY_MS = 1000;
+
+const makeFetchRetryOnError =
+  (
+    fetchFunction: ({}: Theme.GetThemeProductsType) => Promise<Theme.ThemeProductsResponse>,
+    fetchFunctionParams: Theme.GetThemeProductsType,
+    handleResponse: (data: Theme.ThemeProductsResponse) => void,
+    handleError: (e: AxiosError, retryCount: number) => void,
+  ): (() => Promise<void>) =>
+  async () => {
+    let retryCount = 0;
+    let isSuccess = false;
+
+    while (!isSuccess && retryCount < MAX_RETRY_COUNT) {
+      try {
+        const data = await fetchFunction(fetchFunctionParams);
+        handleResponse(data);
+
+        isSuccess = true;
+      } catch (error) {
+        handleError(error as AxiosError, retryCount);
+
+        retryCount += 1;
+        if (!isSuccess && retryCount < MAX_RETRY_COUNT) {
+          await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS));
+        }
+      }
+    }
+  };
 
 export function useGoodsSectionControl(themeKey: string) {
   const [goodsList, setGoodsList] = useState<Home.ProductData[]>([]);
@@ -9,7 +51,7 @@ export function useGoodsSectionControl(themeKey: string) {
   const observerRef = useRef<IntersectionObserver | null>(null);
   const loaderRef = useRef<HTMLDivElement | null>(null);
   const [isError, setIsError] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState<LoadingState>(defaultLoadState);
 
   const handleThemeProductsResponse = (data: Theme.ThemeProductsResponse) => {
     const { products, nextPageToken, pageInfo: ResponsePageInfo } = data;
@@ -25,45 +67,39 @@ export function useGoodsSectionControl(themeKey: string) {
     });
   };
 
-  const fetchThemeProducts = async () => {
-    let isDone = false;
-    let retryCount = 0;
-
-    while (!isDone && retryCount < 5) {
-      try {
-        const data = await getThemeProducts({ themeKey, maxResults: 20 });
-        handleThemeProductsResponse(data);
-        isDone = true;
-      } catch (err) {
-        console.log(err, pageInfo);
-        retryCount += 1;
-
-        if (retryCount === 4) {
-          setIsError(true);
-        } else {
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-        }
-      }
+  const handleError = (error: AxiosError, retryCount: number) => {
+    console.log(error, pageInfo);
+    if (retryCount === MAX_RETRY_COUNT - 1) {
+      setIsError(true);
     }
-    setIsLoading(false);
   };
 
-  useEffect(() => {
-    fetchThemeProducts();
-  }, [themeKey]);
+  const fetchThemeProducts: () => Promise<void> = makeFetchRetryOnError(
+    getThemeProducts,
+    { themeKey, maxResults: 20 },
+    handleThemeProductsResponse,
+    handleError,
+  );
 
   useEffect(() => {
     if (observerRef.current) observerRef.current.disconnect();
 
     if (loaderRef.current) {
       observerRef.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && pageToken) {
-          getThemeProducts({ themeKey, pageToken, maxResults: 20 } as Theme.GetThemeProductsType)
-            .then((data: Theme.ThemeProductsResponse) => handleThemeProductsResponse(data))
-            .catch((err) => {
-              console.error(err, pageInfo);
-              setIsError(true);
-            });
+        if (entries[0].isIntersecting && !pageToken) {
+          fetchThemeProducts().then(() => setIsLoading((prev) => ({ ...prev, isInit: false })));
+        } else if (entries[0].isIntersecting && pageToken) {
+          const fetchNextThemeProducts: () => Promise<void> = makeFetchRetryOnError(
+            getThemeProducts,
+            { themeKey, pageToken, maxResults: 20 },
+            handleThemeProductsResponse,
+            handleError,
+          );
+
+          setIsLoading((prev) => ({ ...prev, loadingMore: true }));
+          fetchNextThemeProducts().then(() =>
+            setIsLoading((prev) => ({ ...prev, loadingMore: false })),
+          );
         }
       });
       observerRef.current.observe(loaderRef.current);
@@ -74,5 +110,5 @@ export function useGoodsSectionControl(themeKey: string) {
     };
   }, [themeKey, pageToken]);
 
-  return { goodsList, loaderRef, isError, isLoading };
+  return { goodsList, loaderRef, isError, isLoading: isLoading.isInit || isLoading.loadingMore };
 }

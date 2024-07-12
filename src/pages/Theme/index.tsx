9f94d228e8f7a169ac00d5acd057782a17b7a705
Dobbymin <gmin0701@knu.ac.kr>
2024-07-12 15:48:22 +0900

    feat(home/theme): context api 적용

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,41 +1,24 @@
-import { useEffect, useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
-import { getThemes } from '@/apis/Theme/theme.api';
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
-import { getCurrentTheme, ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
+import {
+  getCurrentTheme,
+  ThemeHeroSection,
+} from '@/components/features/Theme/ThemeHeroSection';
+import { ThemeProductProvider } from '@/provider/Theme/ThemeProductProvider';
+import { useThemeContext } from '@/provider/Theme/ThemeProvider';
 import { RouterPath } from '@/routes/path';
-
-// import { ThemeMockList } from '@/types/mock';
-
-interface ThemeItem {
-  id: number;
-  key: string;
-  label: string;
-  imageURL: string;
-  title: string;
-  description?: string;
-  backgroundColor: string;
-}
 
 export const ThemePage = () => {
   const { themeKey = '' } = useParams<{ themeKey: string }>();
-  const [themeData, setThemeData] = useState<ThemeItem[]>([]);
+  const { themes, isLoading } = useThemeContext();
 
-  useEffect(() => {
-    const fetchThemes = async () => {
-      const data = await getThemes();
-      setThemeData(data.themes);
-    };
-    fetchThemes();
-  }, []);
+  const currentTheme = getCurrentTheme(themeKey, themes);
 
-  // const currentTheme = getCurrentTheme(themeKey, ThemeMockList);
-  const currentTheme = getCurrentTheme(themeKey, themeData);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-  console.log('themeData: ', themeData);
-  console.log('currentTheme:', currentTheme);
-  console.log('themeKey:', themeKey);
   if (!currentTheme) {
     return <Navigate to={RouterPath.notFound} />;
   }
@@ -43,10 +26,9 @@ export const ThemePage = () => {
   return (
     <>
       <ThemeHeroSection themeKey={themeKey} />
-      <ThemeGoodsSection themeKey={themeKey} />
+      <ThemeProductProvider themeKey={themeKey}>
+        <ThemeGoodsSection themeKey={themeKey} />
+      </ThemeProductProvider>
     </>
   );
 };
-// function useState<T>(arg0: never[]): [any, any] {
-//   throw new Error('Function not implemented.');
-// }

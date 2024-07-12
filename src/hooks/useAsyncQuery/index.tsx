ef90352e05946811e81f2a2ad46d94da85297ac3
박건규 <geongyu09@gmail.com>
2024-07-12 14:17:42 +0900

    refactor: 비동기 fetching 로직의 관심사 분리

---
+++ src/hooks/useAsyncQuery/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+import useQueryState from '../useQueryState';
+
+export default function useAsyncQuery<T, P>(queryFn: (params: P) => Promise<T>, params: P) {
+  const { data, isLoading, error, setData, setIsLoading, setError } = useQueryState<T>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        setData(undefined);
+        setError(undefined);
+        const response = await queryFn(params);
+        setData(response);
+      } catch (e) {
+        setError(e as Error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+    //params를 의존성 배열에 추가하게 되면 무한 루프에 빠지는 것 같습니다..
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [setData, setIsLoading, setError, queryFn]);
+
+  return { data, isLoading, error };
+}
--- src/hooks/useProduct/index.ts
+++ src/hooks/useProduct/index.ts
@@ -1,39 +1,10 @@
-import { useEffect, useState } from 'react';
-
 import mock from '@/apis/index';
 import type { GetThemesParams } from '@/apis/product';
-import type { ProductWithInfo } from '@/types/product';
 
-import useQueryState from '../useQueryState';
+import useAsyncQuery from '../useAsyncQuery';
 
 export default function useProduct(params: Omit<GetThemesParams, 'pageToken'>) {
-  const { data, error, isLoading, setData, setError, setIsLoading } =
-    useQueryState<ProductWithInfo>();
-  const [pageToken, setPageToken] = useState<string>();
-
-  useEffect(() => {
-    (async () => {
-      try {
-        setIsLoading(true);
-        setData(undefined);
-        setError(undefined);
-
-        const response = await mock.getProductWithTheme({
-          ...params,
-          pageToken,
-        });
-
-        setPageToken(response.nextPageToken);
-        setData(response);
-      } catch (e) {
-        console.log(e);
-        setError(e as Error);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [setData, setError, setIsLoading]);
+  const { data, isLoading, error } = useAsyncQuery(mock.getProductWithTheme, params);
 
   return { data, error, isLoading };
 }

ff7dbedcd0ca1549bd96b5f3e1e29812618a6fe4
dlawlghks <ikuwlghks@naver.com>
2024-07-12 16:01:03 +0900

    refactor: HomePage 컴포넌트에 React.FC 타입 명시

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 import { Spacing } from '@/components/common/layouts/Spacing';
 import { AiDiscoveryBanner } from '@/components/features/Home/AiDiscoveryBanner';
 import { GoodsRankingSection } from '@/components/features/Home/GoodsRankingSection';
 import { SelectFriendsBanner } from '@/components/features/Home/SelectFriendsBanner';
 import ThemeCategorySection from '@/components/features/Home/ThemeCategorySection';
 
-export const HomePage = () => {
+export const HomePage: React.FC = () => {
   return (
     <>
       <SelectFriendsBanner />

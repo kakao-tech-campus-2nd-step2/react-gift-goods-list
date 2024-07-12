3b20b3802338e15963a06111106ce817b480b5ae
yunseong0404 <333665@naver.com>
2024-07-12 22:45:56 +0900

    feat: status error 처리

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -8,6 +8,19 @@ export const fetchData = async (path: string, params?: RankingFilterOption) => {
     const response = await axios.get(`${url}${path}`, { params });
     return response.data;
   } catch (error) {
-    console.error('Error fetching themes:', error);
+    if (axios.isAxiosError(error)) {
+      switch (error.response?.status) {
+        case 400:
+          throw new Error('400 error 잘못된 요청입니다.');
+        case 401:
+          throw new Error('401 error 인증이 필요합니다.');
+        case 404:
+          throw new Error('404 error 데이터를 찾을 수 없습니다.');
+        case 500:
+          throw new Error('500 error 서버에 문제가 발생했습니다.');
+        default:
+          throw new Error('error 알 수 없는 오류가 발생했습니다.');
+      }
+    }
   }
 };

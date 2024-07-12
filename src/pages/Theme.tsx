56380b65a64dcaf6dd5ab154214c159be1ff6fe0
joojjang <itchu999@naver.com>
2024-07-12 14:46:26 +0900

    feat(theme): Theme 페이지에 헤더 부분 동적 구현
    - getThemes를 Theme 페이지에서 호출하고 응답에서 필요한 데이터 추출
    - currentTheme을 헤더 컴포넌트(ThemeHeroSection)에 전달하여 구현

--- src/pages/Theme.tsx
+++ src/pages/Theme.tsx
@@ -1,21 +1,55 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+// import { Navigate, useParams } from 'react-router-dom';
 
+import { ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
-import { getCurrentTheme, ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
-import { RouterPath } from '@/routes';
-import { ThemeMockList } from '@/types/mock';
+import { getThemes } from '@/api';
+// import { RouterPath } from '@/routes';
+import { ThemeData } from '@/types';
 
-export const ThemePage = () => {
+export const Theme = () => {
+  const [currentTheme, setCurrentTheme] = useState<ThemeData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
   const { themeKey = '' } = useParams<{ themeKey: string }>();
-  const currentTheme = getCurrentTheme(themeKey, ThemeMockList);
 
-  if (!currentTheme) {
-    return <Navigate to={RouterPath.notFound} />;
-  }
+  useEffect(() => {
+    const fetchThemes = async () => {
+      try {
+        const response = await getThemes();
+        const themes: ThemeData[] = response.themes;
+        // console.log('themes:', themes);
+        const matchedTheme = themes.find((theme: ThemeData) => theme.key === themeKey);
+        // console.log('matched theme:', matchedTheme);
+
+        if (matchedTheme) {
+          setCurrentTheme(matchedTheme);
+          //   console.log('current theme: ', currentTheme);
+        } else {
+          console.error(`No theme found for key: ${themeKey}`);
+          setIsError(true);
+        }
+      } catch (error) {
+        console.error('Error fetching themes:', error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchThemes();
+  }, [themeKey]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error loading themes.</p>;
+  if (!currentTheme) return <p>No current theme</p>;
+  //   if (!currentTheme) return <Navigate to={RouterPath.notFound} />;
 
   return (
     <>
-      <ThemeHeroSection themeKey={themeKey} />
+      <ThemeHeroSection theme={currentTheme} />
       <ThemeGoodsSection themeKey={themeKey} />
     </>
   );

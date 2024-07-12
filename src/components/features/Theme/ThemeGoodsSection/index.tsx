f91c53652b5ec2b38022d021b2ecf08c7990bbb7
rudtj <kyoungseo03@naver.com>
2024-07-12 14:53:56 +0900

    feat: HTTP status에 따른 error 처리

--- src/components/features/Theme/ThemeGoodsSection/index.tsx
+++ src/components/features/Theme/ThemeGoodsSection/index.tsx
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
+import { fetchThemeProducts } from '@/api/Api';
 import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
 import { Container } from '@/components/common/layouts/Container';
 import { Grid } from '@/components/common/layouts/Grid';
@@ -11,7 +14,51 @@ type Props = {
   goods: GoodsData[];
 };
 
-export const ThemeGoodsSection = ({ goods }: Props) => {
+export const ThemeGoodsSection = ({ themeKey }: Props) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+  const [goods, setGoods] = useState<GoodsData[]>([]);
+
+  useEffect(() => {
+    const fetchGoods = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const data = await fetchThemeProducts(themeKey);
+        setGoods(data.products);
+        setLoading(false);
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          switch (err.response?.status) {
+            case 400:
+              setError(new Error('죄송합니다. 요청이 잘못되었습니다. 다시 시도해 주세요.'));
+              break;
+            case 404:
+              setError(new Error('찾을 수 없습니다. 요청하신 페이지를 찾을 수 없습니다.'));
+              break;
+            case 500:
+              setError(new Error('서버에 문제가 발생했습니다. 잠시 후 다시 시도해 주세요.'));
+              break;
+            default:
+              setError(
+                new Error(`알 수 없는 오류가 발생했습니다. 오류 코드: ${err.response?.status}`),
+              );
+              break;
+          }
+        }
+      }
+    };
+    fetchGoods();
+  }, [themeKey]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <Wrapper>
       <Container>

0eddbfde78bda950a97eaad20ebf256b4b1f5972
faddishcorn <juni129461@gmail.com>
2024-07-12 19:57:21 +0900

    refactor: react query 사용에 따른 src/index.tsx 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import '@/styles';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from '@/App';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
+  <QueryClientProvider client={queryClient}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+</QueryClientProvider>
 );

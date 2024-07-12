bcd9b08ae8dbdea7d3247536f95f21afcba96adb
박건규 <geongyu09@gmail.com>
2024-07-12 22:42:16 +0900

    refactor: 에러를 타입가드로서, 만약 기존에 정의된 에러의 경우 새롭게 메시지를 정해더 던지고, 아닌 경우 그대로 던지도록 수정

--- src/apis/errorCode.constants.ts
+++ src/apis/errorCode.constants.ts
@@ -1,9 +1,9 @@
-const ERROR_MESSATE = {
+const ERROR_MESSAGE = {
   400: '잘못된 요청입니다.',
   404: '페이지를 찾을 수 없습니다.',
   500: '서버 에러가 발생했습니다.',
 } as const;
 
-Object.freeze(ERROR_MESSATE);
+Object.freeze(ERROR_MESSAGE);
 
-export default ERROR_MESSATE;
+export default ERROR_MESSAGE;

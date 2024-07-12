d2167542a7551c0f70c830ad1780056d6cb3a6e2
joshuadesu <meron_pan@kakao.com>
2024-07-12 15:45:37 +0900

    Style: Add 스피너 Wrapper

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,16 +1,23 @@
 // import styled from '@emotion/styled';
+import styled from '@emotion/styled'
 import { SyncLoader } from 'react-spinners';
 
 export const Loading = () => {
   return (
-    <SyncLoader 
-      color='#d381ff'
-      loading={true}    // false도 사용 가능!
-      size='20'
-      speedMultiplier={0.8}
+    <LoadingWrapper>
+      <SyncLoader 
+        color='#d381ff'
+        loading={true}    // false도 사용 가능!
+        size='20px'
+        speedMultiplier={0.8}
     />
+    </LoadingWrapper>
   );
 };
 
-// const Spinner = styled.div`
-// `
+const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`

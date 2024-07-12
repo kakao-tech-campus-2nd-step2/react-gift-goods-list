f37d4a4488ac1c82c28ae47122300ebb65ac7bc4
박건규 <geongyu09@gmail.com>
2024-07-12 21:29:19 +0900

    fix: 쿼리키를 수정하여 받아온 props에 대해 새롭게 패칭 되도록 수정

--- src/hooks/querys/useRanking/index.ts
+++ src/hooks/querys/useRanking/index.ts
@@ -3,10 +3,14 @@ import { useQuery } from '@tanstack/react-query';
 import mock from '@/apis/index';
 import type { RankingFilterOption } from '@/types';
 
-export default function useRanking(props: RankingFilterOption) {
+export default function useRanking({ rankType, targetType }: RankingFilterOption) {
   const { data, isLoading, error } = useQuery({
-    queryKey: ['ranking'],
-    queryFn: () => mock.getRanking(props),
+    queryKey: ['ranking', rankType, targetType],
+    queryFn: () =>
+      mock.getRanking({
+        rankType,
+        targetType,
+      }),
   });
   return { data, isLoading, error };
 }

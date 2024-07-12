242cb7fa86e32ab2752ed6054f17001e2225c4fa
faddishcorn <juni129461@gmail.com>
2024-07-12 19:51:02 +0900

    refactor: useGetThemes훅을 react query로 구현

--- src/api/hooks/useGetThemes.ts
+++ src/api/hooks/useGetThemes.ts
@@ -1,27 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import axios from 'axios';
 import type { ThemeData, ThemesResponse } from '@/types';
 
+const fetchThemes = async (): Promise<ThemeData[]> => {
+  const { data } = await axios.get<ThemesResponse>('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/themes');
+  return data.themes;
+};
+
 export const useGetThemes = () => {
-  const [data, setData] = useState<ThemeData[] | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const fetchThemes = async () => {
-      try {
-        const response = await axios.get<ThemesResponse>('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/themes');
-        setData(response.data.themes);
-      } catch (error) {
-        console.error('Error fetching themes:', error);
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchThemes();
-  }, []);
-
-  return { data, isLoading, isError };
+  return useQuery<ThemeData[], Error>('themes', fetchThemes);
 };

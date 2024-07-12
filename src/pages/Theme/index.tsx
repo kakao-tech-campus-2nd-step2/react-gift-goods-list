1ed31c408584eaa17c0c155b955aa262c30fb3fe
2021048695 <hanyanging@hanyang.ac.kr>
2024-07-13 06:14:35 +0900

    feat: Theme 페이지 업데이트

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,22 +1,53 @@
+import { useQuery } from 'react-query';
 import { Navigate, useParams } from 'react-router-dom';
 
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
-import { getCurrentTheme, ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
+import { ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
 import { RouterPath } from '@/routes/path';
-import { ThemeMockList } from '@/types/mock';
+import type { GoodsData, ThemeData } from '@/types';
+import apiClient from '@/utils/api';
+
+const getThemeDetails = async (themeKey: string) => {
+  const response = await apiClient.get<{ themes: ThemeData[] }>('/themes');
+  return response.data.themes.find((theme) => theme.key === themeKey);
+};
+
+const getThemeProducts = async (themeKey: string) => {
+  const response = await apiClient.get<{ products: GoodsData[] }>(`/themes/${themeKey}/products`, {
+    params: {
+      maxResults: 20,
+    },
+  });
+  return response.data.products;
+};
 
 export const ThemePage = () => {
   const { themeKey = '' } = useParams<{ themeKey: string }>();
-  const currentTheme = getCurrentTheme(themeKey, ThemeMockList);
 
-  if (!currentTheme) {
-    return <Navigate to={RouterPath.notFound} />;
+  const {
+    data: currentTheme,
+    isLoading: isThemeLoading,
+    isError: isThemeError,
+  } = useQuery(['theme', themeKey], () => getThemeDetails(themeKey));
+
+  const {
+    data: products,
+    isLoading: isProductsLoading,
+    isError: isProductsError,
+  } = useQuery(['themeProducts', themeKey], () => getThemeProducts(themeKey));
+
+  if (isThemeLoading || isProductsLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isThemeError || isProductsError || !currentTheme) {
+    return <Navigate to={RouterPath.home} />;
   }
 
   return (
     <>
       <ThemeHeroSection themeKey={themeKey} />
-      <ThemeGoodsSection themeKey={themeKey} />
+      <ThemeGoodsSection products={products || []} />
     </>
   );
 };

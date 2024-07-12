15351cc3ca0f6be5ab9c16125e7bb740726abc8a
diwoni <jgw117@naver.com>
2024-07-12 14:17:09 +0900

    refactor: 실시간 선물랭킹 react-query 를 사용해 구현

--- src/components/features/Home/GoodsRankingSection/index.tsx
+++ src/components/features/Home/GoodsRankingSection/index.tsx
@@ -1,16 +1,25 @@
 import styled from '@emotion/styled';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
 import { Container } from '@/components/common/layouts/Container';
 import { Message } from '@/styles';
 import { breakpoints } from '@/styles/variants';
 import type { GoodsData } from '@/types';
-import type { FetchState } from '@/types';
 import { BASE_URL, type RankingFilterOption } from '@/types';
 
 import { GoodsRankingFilter } from './Filter';
 import { GoodsRankingList } from './List';
+
+const fetchRankingList = async (filterOption: RankingFilterOption) => {
+  const params = {
+    targetType: filterOption.targetType,
+    rankType: filterOption.rankType,
+  };
+  const { data } = await axios.get(`${BASE_URL}/api/v1/ranking/products`, { params });
+  return data.products;
+};
 
 export const GoodsRankingSection = () => {
   const [filterOption, setFilterOption] = useState<RankingFilterOption>({
@@ -18,58 +27,21 @@ export const GoodsRankingSection = () => {
     rankType: 'MANY_WISH',
   });
 
-  const [fetchState, setFetchState] = useState<FetchState<GoodsData[]>>({
-    isLoading: true,
-    isError: false,
-    data: null,
-  });
-
-  const [isData, setIsData] = useState(false);
-
-  // filterOption 마다 API 요청 다르게
-  useEffect(() => {
-    const fetchGoodsData = async () => {
-      try {
-        const queryParams = `?targetType=${filterOption.targetType}&rankType=${filterOption.rankType}`;
-        const res = await axios.get(`${BASE_URL}/api/v1/ranking/products${queryParams}`);
-        setFetchState({ isLoading: false, isError: false, data: res.data.products });
-        setIsData(res.data.products.length > 0); // 데이터가 있다면 true
-      } catch (err) {
-        console.error('Error Fetching GoodsData', err);
-        setFetchState({ isLoading: false, isError: true, data: null });
-
-        if (axios.isAxiosError(err)) {
-          switch (err.response?.status) {
-            case 400:
-              console.error('Bad Request');
-              break;
-            case 404:
-              console.error('Not Found');
-              break;
-            case 500:
-              console.error('Internal Server Error');
-              break;
-            default:
-              console.error(`Unknown Error ${err.response?.status}`);
-              break;
-          }
-        }
-      }
-    };
-    fetchGoodsData();
-  }, [filterOption]);
+  const { data, isLoading, isError } = useQuery<GoodsData[]>(['rankingList', filterOption], () =>
+    fetchRankingList(filterOption),
+  );
 
   const renderList = () => {
-    if (fetchState.isError) {
+    if (isError) {
       return <Message>데이터를 불러오는 중에 문제가 발생했습니다.</Message>;
     }
-    if (fetchState.isLoading) {
+    if (isLoading) {
       return <Message>로딩 중...</Message>;
     }
-    if (!isData) {
+    if (!data?.length) {
       return <Message>보여줄 상품이 없습니다!</Message>;
     }
-    return <GoodsRankingList goodsList={fetchState.data} />;
+    return <GoodsRankingList goodsList={data} />;
   };
 
   return (

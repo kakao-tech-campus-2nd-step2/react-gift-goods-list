2eaade2b176aa11a0c545fd79589d31876d7439e
rudtj <kyoungseo03@naver.com>
2024-07-12 14:53:59 +0900

    feat: HTTP status에 따른 error 처리

--- src/components/features/Theme/ThemeHeroSection/index.tsx
+++ src/components/features/Theme/ThemeHeroSection/index.tsx
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
+import { fetchThemes } from '@/api/Api';
 import { Container } from '@/components/common/layouts/Container';
 import { breakpoints } from '@/styles/variants';
 import type { ThemeData } from '@/types';
@@ -9,13 +12,63 @@ type Props = {
   theme: ThemeData;
 };
 
-export const ThemeHeroSection = ({ theme }: Props) => {
+export const ThemeHeroSection = ({ themeKey }: Props) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+  const [themes, setThemes] = useState<ThemeData[]>([]);
+
+  useEffect(() => {
+    const fetchThemeData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const data = await fetchThemes();
+        setThemes(data.themes);
+        setLoading(false);
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          switch (err.response?.status) {
+            case 400:
+              setError(new Error('죄송합니다. 요청이 잘못되었습니다. 다시 시도해 주세요.'));
+              break;
+            case 404:
+              setError(new Error('찾을 수 없습니다. 요청하신 페이지를 찾을 수 없습니다.'));
+              break;
+            case 500:
+              setError(new Error('서버에 문제가 발생했습니다. 잠시 후 다시 시도해 주세요.'));
+              break;
+            default:
+              setError(
+                new Error(`알 수 없는 오류가 발생했습니다. 오류 코드: ${err.response?.status}`),
+              );
+              break;
+          }
+        }
+      }
+    };
+    fetchThemeData();
+  }, []);
+
+  const currentTheme = getCurrentTheme(themeKey, themes);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!currentTheme) {
+    return <div>Theme not found</div>;
+  }
+
   return (
-    <Wrapper backgroundColor={theme.backgroundColor}>
+    <Wrapper backgroundColor={currentTheme.backgroundColor}>
       <Container>
-        <Label>{theme.label}</Label>
-        <Title>{theme.title}</Title>
-        {theme.description && <Description>{theme.description}</Description>}
+        <Label>{currentTheme.label}</Label>
+        <Title>{currentTheme.title}</Title>
+        {currentTheme.description && <Description>{currentTheme.description}</Description>}
       </Container>
     </Wrapper>
   );

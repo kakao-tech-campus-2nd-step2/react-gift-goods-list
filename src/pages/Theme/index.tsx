206df1221e4e857b279bf295397d10938d6ce455
2021048695 <hanyanging@hanyang.ac.kr>
2024-07-13 06:37:37 +0900

    chore: Theme 페이지 업데이트

--- src/pages/Theme/index.tsx
+++ src/pages/Theme/index.tsx
@@ -1,24 +1,15 @@
 import { useQuery } from 'react-query';
 import { Navigate, useParams } from 'react-router-dom';
 
-import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
-import { ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection';
+import { GoodsSection } from '@/components/features/Theme/ThemeGoodsSection';
+import { HeroSection } from '@/components/features/Theme/ThemeHeroSection';
 import { RouterPath } from '@/routes/path';
-import type { GoodsData, ThemeData } from '@/types';
+import type { ThemeData } from '@/types';
 import apiClient from '@/utils/api';
 
 const getThemeDetails = async (themeKey: string) => {
   const response = await apiClient.get<{ themes: ThemeData[] }>('/themes');
   return response.data.themes.find((theme) => theme.key === themeKey);
-};
-
-const getThemeProducts = async (themeKey: string) => {
-  const response = await apiClient.get<{ products: GoodsData[] }>(`/themes/${themeKey}/products`, {
-    params: {
-      maxResults: 20,
-    },
-  });
-  return response.data.products;
 };
 
 export const ThemePage = () => {
@@ -28,26 +19,22 @@ export const ThemePage = () => {
     data: currentTheme,
     isLoading: isThemeLoading,
     isError: isThemeError,
-  } = useQuery(['theme', themeKey], () => getThemeDetails(themeKey));
+  } = useQuery(['theme', themeKey], () => getThemeDetails(themeKey), {
+    enabled: !!themeKey,
+  });
 
-  const {
-    data: products,
-    isLoading: isProductsLoading,
-    isError: isProductsError,
-  } = useQuery(['themeProducts', themeKey], () => getThemeProducts(themeKey));
-
-  if (isThemeLoading || isProductsLoading) {
+  if (isThemeLoading) {
     return <div>Loading...</div>;
   }
 
-  if (isThemeError || isProductsError || !currentTheme) {
+  if (isThemeError || !currentTheme) {
     return <Navigate to={RouterPath.home} />;
   }
 
   return (
     <>
-      <ThemeHeroSection themeKey={themeKey} />
-      <ThemeGoodsSection products={products || []} />
+      <HeroSection />
+      <GoodsSection />
     </>
   );
 };

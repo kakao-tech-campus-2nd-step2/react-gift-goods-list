d54ed785335ec88fa93fc61d1217c46db536b202
joshuadesu <meron_pan@kakao.com>
2024-07-12 15:44:56 +0900

    Feat: Add Trending 선물랭킹 섹션

--- src/components/features/Home/GoodsRankingSection/index.tsx
+++ src/components/features/Home/GoodsRankingSection/index.tsx
@@ -1,19 +1,47 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+import instance from '@/api/api';
 import { Container } from '@/components/common/layouts/Container';
+import { Loading } from '@/components/common/Loading/Loading';
 import { breakpoints } from '@/styles/variants';
-import type { RankingFilterOption } from '@/types';
-import { GoodsMockList } from '@/types/mock';
+import type { GoodsData, RankingFilterOption } from '@/types';
 
 import { GoodsRankingFilter } from './Filter';
 import { GoodsRankingList } from './List';
 
 export const GoodsRankingSection = () => {
+
+  const [goodsList, setGoodsList] = useState<GoodsData[]>([]);
+  const [isLoading, setIsLoading]=useState(true)
+
   const [filterOption, setFilterOption] = useState<RankingFilterOption>({
     targetType: 'ALL',
     rankType: 'MANY_WISH',
   });
+
+  useEffect(()=> {
+    const fetchRankingItems = async () => {
+      try {
+        const { targetType, rankType } = filterOption;
+        const response = await instance.get(
+          `api/v1/ranking/products?targetType=${targetType}&rankType=${rankType}`,
+          );
+          setGoodsList(response.data.products)
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchRankingItems();
+  }, [filterOption]);
+
+  if (isLoading) {
+    return (
+      <Loading />
+    )
+  }
 
   // GoodsMockData를 21번 반복 생성
 
@@ -22,7 +50,7 @@ export const GoodsRankingSection = () => {
       <Container>
         <Title>실시간 급상승 선물랭킹</Title>
         <GoodsRankingFilter filterOption={filterOption} onFilterOptionChange={setFilterOption} />
-        <GoodsRankingList goodsList={GoodsMockList} />
+        <GoodsRankingList goodsList={goodsList} />
       </Container>
     </Wrapper>
   );

e0ca023983e38edc823851a5685dc3e941b2f9d5
faddishcorn <juni129461@gmail.com>
2024-07-12 19:47:50 +0900

    refactor: useRankingGoods훅을 react query로 구현

--- src/api/hooks/useGetRankingGoods.ts
+++ src/api/hooks/useGetRankingGoods.ts
@@ -1,31 +1,18 @@
+import { useQuery } from 'react-query';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { RankingFilterOption, GoodsData } from '@/types';
+import type { RankingFilterOption, GoodsData } from '@/types';
 
 export const useGetRankingGoods = (filterOption: RankingFilterOption) => {
-  const [data, setData] = useState<GoodsData[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isError, setIsError] = useState<boolean>(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      setIsError(false);
-
-      try {
-        const response = await axios.get('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/ranking/products', {
-          params: filterOption,
-        });
-        setData(response.data.products);
-      } catch (error) {
-        setIsError(true);
-      }
-
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, [filterOption]);
-
-  return { data, isLoading, isError };
+  return useQuery<GoodsData[], Error>(
+    ['rankingGoods', filterOption],
+    async () => {
+      const response = await axios.get('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/ranking/products', {
+        params: filterOption,
+      });
+      return response.data.products;
+    },
+    {
+      keepPreviousData: true,
+    }
+  );
 };

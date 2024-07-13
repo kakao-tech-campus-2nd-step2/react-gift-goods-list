8959dc1c1cb84b39f49bd4b0b3f9e4efbfada02b
dmsrud1218 <dmsrud566@naver.com>
2024-07-13 17:44:13 +0900

    chore : app.tsx 원본으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import { Footer } from './components/features/Layout/Footer';
-import { Header } from './components/features/Layout/Header';
-import { HomePage } from './pages/Home';
-//import { LoginPage } from './pages/Login';
-//import { MyAccountPage } from './pages/MyAccount';
-import { ThemePage } from './pages/Theme';
+import { AuthProvider } from './provider/Auth';
+import { Routes } from './routes';
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/theme/:themeKey" element={<ThemePage />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 };
 

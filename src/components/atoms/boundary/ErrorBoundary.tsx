d527d7d3d9164220a429ff207d6dea5926562aaa
cla6shade <cla6shade@gmail.com>
2024-07-13 20:49:57 +0900

    feat: 에러 바운더리 컴포넌트

---
+++ src/components/atoms/boundary/ErrorBoundary.tsx
@@ -0,0 +1,62 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+import axios from 'axios';
+import { ERROR_NOT_DEFINED, ErrorMessages } from '@constants/ErrorMessage';
+import Container from '@components/atoms/container/Container';
+
+interface ErrorBoundaryProps {
+  statusCode: number;
+  hasError: boolean;
+  fallback?: ReactNode;
+  children: ReactNode;
+}
+interface ErrorBoundaryState extends ErrorBoundaryProps {}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      statusCode: props.statusCode,
+      hasError: props.hasError,
+      fallback: props.fallback,
+      children: props.children,
+    };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error(error, errorInfo.componentStack);
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    const ret: {
+      hasError: boolean,
+      statusCode: number,
+    } = {
+      hasError: true,
+      statusCode: ERROR_NOT_DEFINED,
+    };
+
+    if (axios.isAxiosError(error)) {
+      ret.statusCode = error.response?.status || ERROR_NOT_DEFINED;
+    }
+
+    return ret;
+  }
+
+  render() {
+    const {
+      statusCode, hasError, fallback, children,
+    } = this.state;
+
+    if (hasError) {
+      return fallback || (
+        <Container elementSize="full-width" justifyContent="center">
+          <p>{ErrorMessages[statusCode]}</p>
+        </Container>
+      );
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;

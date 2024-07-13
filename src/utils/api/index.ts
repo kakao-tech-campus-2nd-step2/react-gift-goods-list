679507d2eba2f530f4743107f62d4dc3a00b6ed8
wjdsbs <wjdsbss@gmail.com>
2024-07-14 03:01:46 +0900

    refactor: useFetch 훅을 react-query로 바꾸어 구현

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,36 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import { axiosInstance } from '@/api';
 
-interface FetchState<T> {
-  isLoading: boolean;
-  isError: boolean;
-  data: T | null;
-}
-
 const useFetch = <T>(url: string, params: object) => {
-  const [fetchState, setFetchState] = useState<FetchState<T>>({
-    isLoading: true,
-    isError: false,
-    data: null,
+  const queryParams = JSON.stringify(params);
+
+  return useQuery<T, Error>([url, queryParams], async () => {
+    const response = await axiosInstance.get(url, { params });
+    return response.data;
   });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setFetchState({ isLoading: true, isError: false, data: null });
-      try {
-        const response = await axiosInstance.get(url, { params });
-        setFetchState({ isLoading: false, isError: false, data: response.data });
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setFetchState({ isLoading: false, isError: true, data: null });
-      }
-    };
-
-    fetchData();
-  }, [url, params]);
-
-  return fetchState;
 };
 
 export default useFetch;

0e9c9b680df0daf7642b6b865f568eae2c727f5a
cla6shade <cla6shade@gmail.com>
2024-07-14 00:04:39 +0900

    feat: useInView 훅

---
+++ src/hooks/useInView.ts
@@ -0,0 +1,40 @@
+import { RefObject, useEffect, useState } from 'react';
+
+interface UseInViewProps {
+  ref: RefObject<HTMLElement>;
+  threshold: number;
+}
+
+function useInView({ ref, threshold }: UseInViewProps) {
+  const [inView, setInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setInView((prevState) => {
+        console.log(entry.isIntersecting);
+
+        if (prevState === entry.isIntersecting) return prevState;
+
+        return entry.isIntersecting;
+      });
+    }, {
+      threshold,
+    });
+
+    const observeTarget = ref.current;
+
+    if (observeTarget) {
+      observer.observe(observeTarget);
+    }
+
+    return () => { // component unmount시 unobserve
+      if (observeTarget) {
+        observer.unobserve(observeTarget);
+      }
+    };
+  }, [threshold, ref]);
+
+  return { inView };
+}
+
+export default useInView;

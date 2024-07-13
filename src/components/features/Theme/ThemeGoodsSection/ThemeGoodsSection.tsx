7216b43505d53238202144519e2e999191eabdcb
yunseong0404 <114674380+yunseong0404@users.noreply.github.com>
2024-07-13 11:08:37 +0900

    부산대 FE_최윤성_3주차 과제 Step2 (#90)
    * docs: README.md 업데이트
    
    * design: Dual_Ring.gif 추가
    
    * design: error-icon 추가
    
    * feat: status error 처리
    
    * feat: Error UI 컴포넌트 구현
    
    * feat: Loading UI 컴포넌트 구현
    
    * feat: Loading, Error 처리

--- src/components/features/Theme/ThemeGoodsSection/ThemeGoodsSection.tsx
+++ src/components/features/Theme/ThemeGoodsSection/ThemeGoodsSection.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
 import { Container } from '@/components/common/layouts/Container/Container';
 import { Grid } from '@/components/common/layouts/Grid/Grid';
+import ShowError from '@/components/Error/ShowError';
+import Loading from '@/components/Loading/Loading';
 import { breakpoints } from '@/styles/variants';
 import type { product } from '@/types/types';
 import { fetchData } from '@/utils/api/api';
@@ -14,19 +16,35 @@ type Props = {
 
 export const ThemeGoodsSection = ({ themeKey }: Props) => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [fetchError, setFetchError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchThemes = async () => {
+      setLoading(true);
+      setFetchError(null);
       try {
         const response = await fetchData(`/api/v1/themes/${themeKey}/products`);
         const showProducts = response.products.slice(0, 20);
         setProducts(showProducts);
       } catch (error) {
-        console.error('Error fetching themes:', error);
+        setFetchError((error as Error).message);
+      } finally {
+        setLoading(false);
       }
     };
     fetchThemes();
   }, [themeKey]);
+
+  if (loading) {
+    return Loading();
+  }
+  if (fetchError) {
+    return ShowError(fetchError);
+  }
+  if (products.length == 0) {
+    return ShowError('데이터 없음');
+  }
 
   return (
     <Wrapper>
--- src/pages/Theme/ThemePage.tsx
+++ src/pages/Theme/ThemePage.tsx
@@ -2,29 +2,43 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import ShowError from '@/components/Error/ShowError';
 import { ThemeGoodsSection } from '@/components/features/Theme/ThemeGoodsSection/ThemeGoodsSection';
 import { ThemeHeroSection } from '@/components/features/Theme/ThemeHeroSection/ThemeHeroSection';
+import Loading from '@/components/Loading/Loading';
 import type { ThemeData } from '@/types/types';
 import { fetchData } from '@/utils/api/api';
 
 export const ThemePage = () => {
   const { themeKey = '' } = useParams<{ themeKey: string }>();
   const [themes, setThemes] = useState<ThemeData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [fetchError, setFetchError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchThemes = async () => {
+      setLoading(true);
+      setFetchError(null);
       try {
         const response = await fetchData('/api/v1/themes');
         setThemes(response.themes);
       } catch (error) {
-        console.error('Error fetching themes:', error);
+        setFetchError((error as Error).message);
+      } finally {
+        setLoading(false);
       }
     };
     fetchThemes();
   }, [themeKey]);
 
-  if (themes.length === 0) {
-    return <div>no Data</div>;
+  if (loading) {
+    return Loading();
+  }
+  if (fetchError) {
+    return ShowError(fetchError);
+  }
+  if (themes.length == 0) {
+    return ShowError('데이터 없음');
   }
 
   return (

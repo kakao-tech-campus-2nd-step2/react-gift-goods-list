537d8d9157d31b7d9c3fb41a2cc71c88465b7603
TAEHONG-LEE <2t_hong@naver.com>
2024-07-15 05:12:17 +0900

    feat: implement SectionHeader component using /api/v1/themes API

--- src/components/features/Home/ThemeCategorySection/index.tsx
+++ src/components/features/Home/ThemeCategorySection/index.tsx
@@ -1,105 +1,49 @@
+import React from 'react';
 import styled from '@emotion/styled';
+import { ThemesResponse } from '@/types/responseTypes';
+import { getThemes } from '@apis/themes';
+import useFetch from '@hooks/useFetch';
+import { Grid, CenteredContainer, Spinner, ErrorMessage } from '@components/common';
 import { Link } from 'react-router-dom';
+import { ROUTE_PATH } from '@routes/path';
+import { getDynamicPath } from '@utils/getDynamicPath';
+import ThemeItem from './ThemeItem';
 
-import { Container } from '@/components/common/layouts/Container';
-import { Grid } from '@/components/common/layouts/Grid';
-import { getDynamicPath } from '@/routes/path';
-import { breakpoints } from '@/styles/variants';
+const GRID_GAP = 0;
+const GRID_COLUMNS = 6;
+const ERROR_MESSAGE = '데이터를 불러오는 중에 문제가 발생했습니다.';
 
-import { ThemeCategoryItem } from './ThemeCategoryItem';
+export default function ThemeCategory() {
+  const { isLoading, isError, data } = useFetch<ThemesResponse>(getThemes);
 
-export const ThemeCategorySection = () => {
+  if (isLoading) return <Spinner />;
+  if (isError) return <ErrorMessage message={ERROR_MESSAGE} />;
+
   return (
-    <Wrapper>
-      <Container>
-        <Grid
-          columns={{
-            initial: 4,
-            md: 6,
-          }}
-        >
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="생일"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="졸업선물"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="스몰럭셔리"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="명품선물"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="결혼/집들이"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="따뜻한선물"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="가벼운선물"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="팬심저격"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="교환권"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="건강/비타민"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="과일/한우"
-            />
-          </Link>
-          <Link to={getDynamicPath.theme('life_small_gift')}>
-            <ThemeCategoryItem
-              image="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-              label="출산/키즈"
-            />
-          </Link>
+    <ThemeCategoryContainer>
+      <CenteredContainer maxWidth="md">
+        <Grid gap={GRID_GAP} columns={GRID_COLUMNS}>
+          {data?.themes.map((theme) => (
+            <Link
+              key={theme.id}
+              to={getDynamicPath(ROUTE_PATH.THEME, { themeKey: theme.key })}
+              state={{
+                backgroundColor: theme.backgroundColor,
+                label: theme.label,
+                title: theme.title,
+                description: theme.description,
+              }}
+            >
+              <ThemeItem image={theme.imageURL} label={theme.label} />
+            </Link>
+          ))}
         </Grid>
-      </Container>
-    </Wrapper>
+      </CenteredContainer>
+    </ThemeCategoryContainer>
   );
-};
+}
 
-const Wrapper = styled.section`
-  padding: 14px 14px 3px;
-
-  @media screen and (min-width: ${breakpoints.sm}) {
-    padding: 45px 52px 23px;
-  }
+const ThemeCategoryContainer = styled.section`
+  padding-top: 45px;
+  padding-bottom: 23px;
 `;

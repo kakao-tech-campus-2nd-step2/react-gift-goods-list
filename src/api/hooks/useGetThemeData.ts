70a781b0435a7c5b54b60fa64b8c86abe46f9e2a
wjdsbs <wjdsbss@gmail.com>
2024-07-14 16:33:42 +0900

    refactor: useQuery의 옵션을 사용하는 곳에서 결정할 수 있도록 각 hooks에 useQueryOptions 추가

--- src/api/hooks/useGetThemeData.ts
+++ src/api/hooks/useGetThemeData.ts
@@ -1,3 +1,4 @@
+import type { UseQueryOptions } from 'react-query';
 import { useQuery } from 'react-query';
 
 import { axiosInstance } from '@/api';
@@ -7,16 +8,23 @@ export const getCurrentTheme = (themeKey: string, themeList: ThemeData[]) => {
   return themeList.find((theme) => theme.key === themeKey);
 };
 
-export const useGetThemeData = (themeKey: string) => {
-  return useQuery<ThemeData, Error>(['theme', themeKey], async () => {
-    const response = await axiosInstance.get('/api/v1/themes');
-    const themeList: ThemeData[] = response.data.themes;
-    const theme = getCurrentTheme(themeKey, themeList);
-    if (!theme) {
-      throw new Error('Theme not found');
-    }
-    return theme;
-  });
+export const useGetThemeData = (
+  themeKey: string,
+  queryOptions?: UseQueryOptions<ThemeData, Error>,
+) => {
+  return useQuery<ThemeData, Error>(
+    ['theme', themeKey],
+    async () => {
+      const response = await axiosInstance.get('/api/v1/themes');
+      const themeList: ThemeData[] = response.data.themes;
+      const theme = getCurrentTheme(themeKey, themeList);
+      if (!theme) {
+        throw new Error('Theme not found');
+      }
+      return theme;
+    },
+    queryOptions,
+  );
 };
 
 export default useGetThemeData;
--- src/api/hooks/useGetThemes.ts
+++ src/api/hooks/useGetThemes.ts
@@ -1,11 +1,16 @@
+import type { UseQueryOptions } from 'react-query';
 import { useQuery } from 'react-query';
 
 import { axiosInstance } from '@/api/index';
 import type { ThemeData } from '@/types/index';
 
-export const useGetThemes = () => {
-  return useQuery<ThemeData[]>('themes', async () => {
-    const response = await axiosInstance.get('/api/v1/themes');
-    return response.data.themes;
-  });
+export const useGetThemes = (queryOptions?: UseQueryOptions<ThemeData[]>) => {
+  return useQuery<ThemeData[]>(
+    'themes',
+    async () => {
+      const response = await axiosInstance.get('/api/v1/themes');
+      return response.data.themes;
+    },
+    queryOptions,
+  );
 };

7b581f96e14f3bb609b738cd6ce98cd122006441
HyeonChae1104 <211063@jnu.ac.kr>
2024-07-15 03:00:43 +0900

    feat: react-query 설치 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { AuthProvider } from './provider/Auth';
 import { Routes } from './routes';
 
 const App = () => {
+  const queryClient = new QueryClient();
   return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 

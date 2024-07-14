4ac9777d3f6453e6ca70ffcf158ee2cef33431dd
wjdsbs <wjdsbss@gmail.com>
2024-07-14 17:08:15 +0900

    feat: react-query devtools 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import '@/styles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import App from '@/App';
 
@@ -14,6 +15,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>,
 );

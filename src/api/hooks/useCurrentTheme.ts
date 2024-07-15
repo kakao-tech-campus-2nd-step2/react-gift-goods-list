8f0a89ffe26871e2557fa5c99b9105c316efe41f
faddishcorn <juni129461@gmail.com>
2024-07-15 21:50:27 +0900

    refactor: useCurrentTheme에서 fetchData유틸 사용

--- src/api/hooks/useCurrentTheme.ts
+++ src/api/hooks/useCurrentTheme.ts
@@ -1,5 +1,5 @@
 import { useQuery } from 'react-query';
-import axios from 'axios';
+import { fetchData } from '@/api/utils/fetchData';
 import type { ThemeData } from '@/types';
 
 type UseCurrentThemeProps = {
@@ -8,21 +8,15 @@ type UseCurrentThemeProps = {
 
 export const useCurrentTheme = ({ themeKey }: UseCurrentThemeProps) => {
   return useQuery<ThemeData, Error>(['currentTheme', themeKey], async () => {
-    const response = await axios.get('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/themes')
-      .then((response) => response)
-      .catch((error) => {
-        console.error("fetch에 실패하였습니다:", error);
-        throw new Error('theme data fetch 실패');
-      });
+    const response = await fetchData<{ themes: ThemeData[] }>('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/themes');
 
     // 추가적인 검증 로직
-    if (!response || !response.data || !Array.isArray(response.data.themes)) {
-      throw new Error('유효하지않은 response');
+    const themes = response.themes;
+    if (!themes || !Array.isArray(themes)) {
+      throw new Error('유효하지 않은 response');
     }
 
-    const themes: ThemeData[] = response.data.themes;
     const theme = themes.find(t => t.key === themeKey);
-
     if (!theme) {
       throw new Error('Theme not found');
     }

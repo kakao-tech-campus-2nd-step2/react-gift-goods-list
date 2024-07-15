e760602810778b6510f4ff2827d51894a91df451
faddishcorn <juni129461@gmail.com>
2024-07-15 19:00:07 +0900

    feat: useCurrentTheme훅에대한 에러 대응 로직 추가

--- src/api/hooks/useCurrentTheme.ts
+++ src/api/hooks/useCurrentTheme.ts
@@ -8,7 +8,18 @@ type UseCurrentThemeProps = {
 
 export const useCurrentTheme = ({ themeKey }: UseCurrentThemeProps) => {
   return useQuery<ThemeData, Error>(['currentTheme', themeKey], async () => {
-    const response = await axios.get('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/themes');
+    const response = await axios.get('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/themes')
+      .then((response) => response)
+      .catch((error) => {
+        console.error("fetch에 실패하였습니다:", error);
+        throw new Error('theme data fetch 실패');
+      });
+
+    // 추가적인 검증 로직
+    if (!response || !response.data || !Array.isArray(response.data.themes)) {
+      throw new Error('유효하지않은 response');
+    }
+
     const themes: ThemeData[] = response.data.themes;
     const theme = themes.find(t => t.key === themeKey);
 

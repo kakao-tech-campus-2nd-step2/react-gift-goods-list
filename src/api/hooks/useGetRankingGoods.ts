25f5d9629fa7f946b054964b65745a8bc19c2307
faddishcorn <juni129461@gmail.com>
2024-07-16 04:01:18 +0900

    fix: fetch오류수정

--- src/api/hooks/useGetRankingGoods.ts
+++ src/api/hooks/useGetRankingGoods.ts
@@ -1,19 +1,26 @@
 import { useQuery } from 'react-query';
+import axios from 'axios';
 import type { RankingFilterOption, GoodsData } from '@/types';
-import { fetchData } from '@/api/utils/fetchData';
 
 export const useGetRankingGoods = (filterOption: RankingFilterOption) => {
   return useQuery<GoodsData[], Error>(
     ['rankingGoods', filterOption],
     async () => {
-      const data = await fetchData<GoodsData[]>('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/ranking/products', {
-        params: filterOption,
-      });
-      // 추가 검증
-      if (!data || !Array.isArray(data)) {
-        throw new Error('유효하지 않은 response');
+      try {
+        const response = await axios.get('https://react-gift-mock-api-git-main-faddishcorns-projects.vercel.app/api/v1/ranking/products', {
+          params: filterOption,
+        });
+
+        // 추가검증
+        if (!response || !response.data || !Array.isArray(response.data.products)) {
+          throw new Error('유효하지 않은 response');
+        }
+
+        return response.data.products;
+      } catch (error) {
+        console.error('ranking goods fetching에 실패하였습니다.:', error);
+        throw new Error('fetching ranking goods 실패');
       }
-      return data;
     },
     {
       keepPreviousData: true,

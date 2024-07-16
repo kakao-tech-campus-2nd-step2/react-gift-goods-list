65803f3592bf446dc68db25138d91f3d80fb3c95
Eric <real.dassy@gmail.com>
2024-07-16 22:20:48 +0900

    feat: QueryClientProvider 추가
    react-query 코드 적용을 위한 provider를 추가했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { ThemeProvider } from '@/context/ThemeContext';
 
 import { AuthProvider } from './provider/Auth';
 import { Routes } from './routes';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <AuthProvider>
-      <ThemeProvider>
-        <Routes />
-      </ThemeProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeProvider>
+          <Routes />
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 

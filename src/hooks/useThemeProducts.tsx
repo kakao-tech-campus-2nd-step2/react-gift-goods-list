c038cdb08e5ef9688a0f8f36d4604c62fa4663ee
mingkyeongg <ming0820@naver.com>
2024-07-16 15:05:18 +0900

    refactor: create createErrorMessage utility

--- src/hooks/useThemeProducts.tsx
+++ src/hooks/useThemeProducts.tsx
@@ -1,8 +1,9 @@
-import type { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import { useEffect,useState } from 'react';
 
 import apiClient from '@/api';
 import type { ProductData,ThemeProductsResponse } from '@/types';
+import createErrorMessage from '@/utils/createErrorMessage';
 
 interface FetchState<T> {
   isLoading: boolean;
@@ -29,29 +30,8 @@ export const useThemeProducts = (themeKey: string) => {
         });
         setFetchState({ isLoading: false, isError: false, data: response.data.products, errorMessage: null});
       } catch (err) {
-		const error = err as AxiosError;
-		let errorMessage = 'An unknown error occurred';
-		if (error.response) {
-			switch (error.response.status) {
-				case 400:
-					errorMessage = '400 Bad Request: 요청이 올바르지 않습니다.';
-					break;
-				case 401:
-					errorMessage = '401 Unauthorized: 인증이 필요합니다.';
-					break;
-				case 403:
-					errorMessage = '403 Forbidden: 접근 권한이 없습니다.';
-					break;
-				case 404:
-					errorMessage = '404 Not Found: 요청한 리소스를 찾을 수 없습니다.';
-					break;
-				case 500:
-					errorMessage = '500 Internal Server Error: 서버에 오류가 발생했습니다.';
-					break;
-				default:
-					errorMessage = `Unexpected error: ${error.response.status}`;
-			}
-		}
+        const error = err as AxiosError;
+        const errorMessage = error instanceof AxiosError ? createErrorMessage(error.response) : 'An unknown error occurred';
         console.error(error);
         setFetchState({ isLoading: false, isError: true, data: null, errorMessage: errorMessage});
       }
--- src/hooks/useThemes.tsx
+++ src/hooks/useThemes.tsx
@@ -1,8 +1,9 @@
-import type { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import { useEffect,useState } from 'react';
 
 import apiClient from '@/api/index';
 import type { ThemeData,ThemesResponse } from '@/types';
+import createErrorMessage from '@/utils/createErrorMessage';
 
 interface FetchState<T> {
   isLoading: boolean;
@@ -27,28 +28,7 @@ export const useThemes = () => {
       } catch (err) {
 		const error = err as AxiosError;
         console.error(error);
-		let errorMessage = 'An unknown error occurred';
-		if (error.response) {
-			switch (error.response.status) {
-				case 400:
-					errorMessage = '400 Bad Request: 요청이 올바르지 않습니다.';
-					break;
-				case 401:
-					errorMessage = '401 Unauthorized: 인증이 필요합니다.';
-					break;
-				case 403:
-					errorMessage = '403 Forbidden: 접근 권한이 없습니다.';
-					break;
-				case 404:
-					errorMessage = '404 Not Found: 요청한 리소스를 찾을 수 없습니다.';
-					break;
-				case 500:
-					errorMessage = '500 Internal Server Error: 서버에 오류가 발생했습니다.';
-					break;
-				default:
-					errorMessage = `Unexpected error: ${error.response.status}`;
-			}
-		}
+		const errorMessage = error instanceof AxiosError ? createErrorMessage(error.response) : 'An unknown error occurred';
         setFetchState({ isLoading: false, isError: true, data: null, errorMessage: errorMessage});
       }
     };
